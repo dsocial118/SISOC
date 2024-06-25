ded02014b45c6a452a3c47a8dbcb3079a33e292c
MariaNavarro90 <navarromariavictoria8@gmail.com>
2024-06-25 11:15:53 -0300

    Refactor legajos dimensiones

--- static/custom/js/legajosdimensionesform.js
+++ static/custom/js/legajosdimensionesform.js
@@ -3,12 +3,14 @@ document.addEventListener("DOMContentLoaded", function() {
     const INCOMPLETO_OPTION = "Incompleto";
     var selects = document.querySelectorAll('select[name="areaCurso"], select[name="areaOficio"]');
     var maxSelections = 3;
+    //Variables dimensión Educación
 
-    let idIncompletoFormParent = document.querySelector("#div_id_nivelIncompleto").parentNode.parentNode;
-    let sinEduFormalParent = document.querySelector("#div_id_sinEduFormal").parentNode.parentNode;
-    let areaCursoParent = document.querySelector("#div_id_areaCurso").parentNode.parentNode;
-    let maxNivelParent = document.querySelector("#div_id_max_nivel").parentNode.parentNode;
-    let estadoNivelParent = document.querySelector("#div_id_estado_nivel").parentNode.parentNode;
+    const ID_INCOMPLETO_FORM ="#div_id_nivelIncompleto";
+    const ID_SIN_EDU_FORMAL = "#div_id_sinEduFormal";
+    const ID_AREA_CURSO = "#div_id_areaCurso";
+    const ID_MAX_NIVEL = "#div_id_max_nivel";
+    const ID_ESTADO_NIVEL = "#div_id_estado_nivel";
+    // Fin var educación
 
     selects.forEach(function(select) {
         select.setAttribute('multiple', 'multiple');
@@ -25,30 +27,41 @@ document.addEventListener("DOMContentLoaded", function() {
     let estadoNivelForm = document.querySelector('#id_estado_nivel');
     let asisteEscuelaForm = document.querySelector('#id_asiste_escuela');
 
+    function mostrar (id, mostrar = true){
+      elemento = document.getElementById(id).parentNode.parentNode
+      if (mostrar == true){
+        elemento.classList.remove("hide")
+      }
+      else {
+        elemento.classList.add("hide")
+      }
+    }
+
+
     function esconderPreguntasForm()
     {
-      idIncompletoFormParent.classList.add("hide");
-      sinEduFormalParent.classList.add("hide");
+      mostrar(ID_INCOMPLETO_FORM, false);
+      mostrar(ID_SIN_EDU_FORMAL, false);
     }
     
     function asisteEscuelaChoices(value){
       if(value == "a"){
-        maxNivelParent.classList.remove("hide");
-        estadoNivelParent.classList.remove("hide");
-        idIncompletoFormParent.classList.add("hide");
-        sinEduFormalParent.classList.add("hide");
+        mostrar(ID_MAX_NIVEL);
+        mostrar(ID_ESTADO_NIVEL);
+        mostrar(ID_INCOMPLETO_FORM, false);
+        mostrar(ID_SIN_EDU_FORMAL, false);
       }
       else if(value == "b"){
-        maxNivelParent.classList.remove("hide");
-        estadoNivelParent.classList.remove("hide");
-        idIncompletoFormParent.classList.remove("hide");
-        sinEduFormalParent.classList.add("hide");
+        mostrar(ID_MAX_NIVEL);
+        mostrar(ID_ESTADO_NIVEL);
+        mostrar(ID_INCOMPLETO_FORM);
+        mostrar(ID_SIN_EDU_FORMAL, false);
       }
       else if(value == "c"){
-        maxNivelParent.classList.add("hide");
-        estadoNivelParent.classList.add("hide");
-        sinEduFormalParent.classList.remove("hide");
-        idIncompletoFormParent.classList.add("hide");
+        mostrar(ID_MAX_NIVEL);
+        mostrar(ID_ESTADO_NIVEL);
+        mostrar(ID_SIN_EDU_FORMAL, false);
+        mostrar(ID_INCOMPLETO_FORM);
 
       }
     }
@@ -69,8 +82,8 @@ document.addEventListener("DOMContentLoaded", function() {
     estadoNivelForm.addEventListener('change', function(event) {
       if (event.target.value == INCOMPLETO_OPTION)
       {
-        idIncompletoFormParent.classList.remove("hide");
-        sinEduFormalParent.classList.remove("hide");
+        mostrar(ID_INCOMPLETO_FORM);
+        mostrar(ID_SIN_EDU_FORMAL);
       }
       else
       {
@@ -79,13 +92,13 @@ document.addEventListener("DOMContentLoaded", function() {
     });
     let realizandoCursoForm = document.querySelector('#id_realizandoCurso');
     if (realizandoCursoForm.value == "False"){
-      areaCursoParent.classList.add("hide");
+      mostrar(ID_AREA_CURSO, false);
     }
     realizandoCursoForm.addEventListener('change',function(event){
       if ( event.target.value == 'True')
-        areaCursoParent.classList.remove("hide"); //classList.remove es para eliminar una clase de un elemento
+        mostrar(ID_AREA_CURSO); //classList.remove es para eliminar una clase de un elemento
       else{
-        areaCursoParent.classList.add("hide"); //classList.add es para agregar una clase a un elemento
+        mostrar(ID_AREA_CURSO, false); //classList.add es para agregar una clase a un elemento
         //la clase hide esconde visualmente a un elemento en el DOM
         //event.taget es el elemento al que aplica el listener.
       }
