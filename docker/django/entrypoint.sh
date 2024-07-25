186fe48f346e65eed4711c80742e1ab31cb27b58
juanikitro <jpkitro@gmail.com>
2024-07-25 12:17:20 -0300

    15 - Implementacion de mysql y django en docker-compose

---
+++ docker/django/entrypoint.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -e
+
+echo "Waiting for MySQL to be ready..."
+while ! mysqladmin ping -h"$SQL_HOST" --silent; do
+    sleep 5
+done
+
+# Add a delay to ensure MySQL dump is fully processed
+sleep 10
+
+echo "MySQL is up and ready"
+
+# Migrate and start the Django server
+python manage.py migrate --noinput
+python manage.py runserver 0.0.0.0:8000
