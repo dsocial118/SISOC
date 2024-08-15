99f62ce34548e550cded219ee91db74e91d83ce0
MariaNavarro90 <navarromariavictoria8@gmail.com>
2024-08-15 16:16:43 -0300

    fix migracion 011

--- Legajos/migrations/0011_dimensioneconomia_legajos_dim_fk_lega_049308_idx.py
+++ Legajos/migrations/0011_dimensioneconomia_legajos_dim_fk_lega_049308_idx.py
@@ -3,7 +3,7 @@
 from django.db import migrations, models
 
 
-class Migration(migrations.Migration)
+class Migration(migrations.Migration):
 
     dependencies = [
         ('Legajos', '0010_dimensiontrabajo_legajos_dim_fk_lega_bd11d0_idx'),
