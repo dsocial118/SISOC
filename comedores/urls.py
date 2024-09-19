c98b9a363b4f35fe1d20f4ccce52136defe65078
juanikitro <jpkitro@gmail.com>
2024-09-19 17:14:37 -0300

    #58 - Implementacion ComedorCreateView

--- comedores/urls.py
+++ comedores/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from django.contrib.auth.decorators import login_required
 
-from comedores.views import ComedorListView, ComedorCreateView
+from comedores.views import ComedorListView, ComedorCreateView, ComedorDetailView
 
 
 urlpatterns = [
@@ -16,6 +16,21 @@ urlpatterns = [
         name="comedor_crear",
     ),
     path(
+        "comedores/ver/<pk>",
+        login_required(ComedorDetailView.as_view()),
+        name="comedor_ver",
+    ),
+    path(
+        "comedores/editar/<pk>",
+        login_required(ComedorDetailView.as_view()),
+        name="comedor_editar",
+    ),
+    path(
+        "comedores/eliminar/<pk>",
+        login_required(ComedorDetailView.as_view()),
+        name="comedor_eliminar",
+    ),
+    path(
         "comedores/<pk>/relevamientos",
         login_required(ComedorListView.as_view()),
         name="comedor_relevamientos",
@@ -24,10 +39,5 @@ urlpatterns = [
         "comedores/<pk>/observaciones",
         login_required(ComedorListView.as_view()),
         name="comedor_observaciones",
-    ),
-    path(
-        "comedores/<pk>/observaciones",
-        login_required(ComedorListView.as_view()),
-        name="comedor_ver",
     ),
 ]
