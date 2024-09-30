8aac989a214469cea945a93e8ac2527fcb17831a
juanikitro <jpkitro@gmail.com>
2024-09-30 13:36:52 -0300

    #58 - Refactorice la vista de comedores

---
+++ comedores/services/comedor_service.py
@@ -0,0 +1,60 @@
+from typing import Union
+from django.db.models import Q
+from comedores.models import Comedor
+
+
+class ComedorService:
+    @staticmethod
+    def get_comedores_filtrados(query: Union[str, None] = None):
+        queryset = Comedor.objects.prefetch_related("provincia", "referente").values(
+            "id",
+            "nombre",
+            "provincia__nombre",
+            "municipio__nombre_region",
+            "localidad__nombre",
+            "barrio",
+            "partido",
+            "calle",
+            "numero",
+            "referente__nombre",
+            "referente__apellido",
+            "referente__celular",
+        )
+        if query:
+            queryset = queryset.filter(
+                Q(nombre__icontains=query)
+                | Q(provincia__nombre__icontains=query)
+                | Q(municipio__nombre_region=query)
+                | Q(localidad__nombre=query)
+                | Q(referente__nombre=query)
+                | Q(referente__apellido=query)
+                | Q(referente__celular=query)
+            )
+        return queryset
+
+    @staticmethod
+    def get_comedor_detail_object(comedor_id: int):
+        return (
+            Comedor.objects.select_related("provincia", "referente")
+            .values(
+                "id",
+                "nombre",
+                "comienzo",
+                "provincia__nombre",
+                "municipio__nombre_region",
+                "localidad__nombre",
+                "partido",
+                "barrio",
+                "calle",
+                "numero",
+                "entre_calle_1",
+                "entre_calle_2",
+                "codigo_postal",
+                "referente__nombre",
+                "referente__apellido",
+                "referente__mail",
+                "referente__celular",
+                "referente__documento",
+            )
+            .get(pk=comedor_id)
+        )
