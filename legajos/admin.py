8462409eddec0309488bda126e085187bab66b2f
Nenu <nehuennavarro@gmail.com>
2024-10-02 12:07:02 -0300

    blabk y djlint

--- legajos/admin.py
+++ legajos/admin.py
@@ -4,26 +4,20 @@ from .models import LegajoProvincias, LegajoMunicipio, LegajoLocalidad
 
 @admin.register(LegajoProvincias)
 class LegajoProvinciasAdmin(admin.ModelAdmin):
-    list_display = (
-        "nombre",
-    )
+    list_display = ("nombre",)
     search_fields = ["nombre"]
     ordering = ["nombre"]
 
 
 @admin.register(LegajoMunicipio)
 class LegajoMunicipioAdmin(admin.ModelAdmin):
-    list_display = (
-        "nombre",
-    )
+    list_display = ("nombre",)
     search_fields = ["nombre"]
     ordering = ["nombre"]
 
 
 @admin.register(LegajoLocalidad)
 class LegajoLocalidadAdmin(admin.ModelAdmin):
-    list_display = (
-        "nombre",
-    )
+    list_display = ("nombre",)
     search_fields = ["nombre"]
     ordering = ["nombre"]
--- legajos/views.py
+++ legajos/views.py
@@ -93,9 +93,7 @@ ROL_ADMIN = "usuarios.rol_admin"
 
 def load_municipios(request):
     provincia_id = request.GET.get("provincia_id")
-    municipios = LegjosDepatamento.objects.filter(
-        fk_provincia=provincia_id
-    )
+    municipios = LegjosDepatamento.objects.filter(fk_provincia=provincia_id)
     return JsonResponse(list(municipios.values("id", "nombre")), safe=False)
 
 
@@ -104,30 +102,24 @@ def load_localidad(request):
     departamento_id = request.GET.get("departamento_id")
 
     if municipio_id:
-        localidades = LegajoLocalidad.objects.filter(
-            fk_municipio=municipio_id
-        )
+        localidades = LegajoLocalidad.objects.filter(fk_municipio=municipio_id)
     else:
-        localidades = LegajoLocalidad.objects.filter(
-            fk_departamento=departamento_id
-        )
+        localidades = LegajoLocalidad.objects.filter(fk_departamento=departamento_id)
     return JsonResponse(list(localidades.values("id", "nombre")), safe=False)
+
 
 def load_departamento(request):
     provincia_id = request.GET.get("provincia_id")
-    departamentos = LegjosDepatamento.objects.filter(
-        fk_provincia=provincia_id
-    )
+    departamentos = LegjosDepatamento.objects.filter(fk_provincia=provincia_id)
     return JsonResponse(list(departamentos.values("id", "nombre")), safe=False)
+
 
 def load_asentamiento(request):
     municipio_id = request.GET.get("municipio_id")
     departamento_id = request.GET.get("departamento_id")
 
     if municipio_id:
-        asentamientos = LegajoAsentamientos.objects.filter(
-            fk_localidad=municipio_id
-        )
+        asentamientos = LegajoAsentamientos.objects.filter(fk_localidad=municipio_id)
     else:
         asentamientos = LegajoAsentamientos.objects.filter(
             fk_departamento=departamento_id
