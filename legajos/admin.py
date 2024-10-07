1f3980dd017e882b3ee56e27482dd31dd40f098a
MariaNavarro90 <navarromariavictoria8@gmail.com>
2024-10-07 16:40:31 -0300

    #48 Creados los admin para manejar los choices de dimension vivienda

--- legajos/admin.py
+++ legajos/admin.py
@@ -12,6 +12,21 @@ from .models import (
     TipoGestion,
     Grado,
     Turno,
+    CantidadAmbientes,
+    CondicionDe,
+    ContextoCasa,
+    TipoAyudaHogar,
+    TipoVivienda,
+    TipoPosesionVivienda,
+    TipoPisosVivienda,
+    TipoTechoVivienda,
+    Agua,
+    Desague,
+    Inodoro,
+    Gas,
+    TipoConstruccionVivienda,
+    TipoEstadoVivienda,
+    
 )
 
 
@@ -45,3 +60,17 @@ admin.site.register(AreaCurso)
 admin.site.register(TipoGestion)
 admin.site.register(Grado)
 admin.site.register(Turno)
+admin.site.register(CantidadAmbientes)
+admin.site.register(CondicionDe)
+admin.site.register(ContextoCasa)
+admin.site.register(TipoAyudaHogar)
+admin.site.register(TipoVivienda)
+admin.site.register(TipoPosesionVivienda)
+admin.site.register(TipoPisosVivienda)
+admin.site.register(TipoTechoVivienda)
+admin.site.register(Agua)
+admin.site.register(Desague)
+admin.site.register(Inodoro)
+admin.site.register(Gas)
+admin.site.register(TipoConstruccionVivienda)
+admin.site.register(TipoEstadoVivienda)
