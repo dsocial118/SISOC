cde7679da0c2591219f68f96aad4afe807c3298b
juanikitro <jpkitro@gmail.com>
2024-10-15 12:43:17 -0300

    #58 - send_to_gestionar de comedor y relevamiento

--- comedores/signals.py
+++ comedores/signals.py
@@ -1,10 +1,17 @@
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-from comedores.models import Relevamiento
+from comedores.models import Relevamiento, Comedor
 from comedores.services.relevamiento_service import RelevamientoService
+from comedores.services.comedor_service import ComedorService
+
+
+@receiver(post_save, sender=Comedor)
+def send_comedor_to_gestionar(sender, instance, created, **kwargs):
+    if created:
+        ComedorService.send_to_gestionar(instance)
 
 
 @receiver(post_save, sender=Relevamiento)
-def post_save_mi_modelo(sender, instance, created, **kwargs):
+def send_relevamiento_to_gestionar(sender, instance, created, **kwargs):
     if created:
         RelevamientoService.send_to_gestionar(instance)
