d12e9d3168d6098bc95f058476e47c37e615069b
MariaNavarro90 <navarromariavictoria8@gmail.com>
2024-10-15 08:50:02 -0300

    #48 Refactor del código para usar el modelo EstadoDerivacion para filtrar legojosderivaciones estado

--- dashboard/utils.py
+++ dashboard/utils.py
@@ -5,7 +5,7 @@ from django.db import connection
 from django.db.models import Count, Q
 
 from configuraciones.models import Alertas
-from legajos.models import LegajoAlertas, Legajos, LegajosDerivaciones
+from legajos.models import LegajoAlertas, Legajos, LegajosDerivaciones, EstadoDerivacion
 
 # FIXME: Este modulo puede ser optimizado mas aun
 
@@ -66,7 +66,7 @@ def obtener_legajos_counts():
                 ),
                 sin_aceptadas=Count(
                     "id",
-                    filter=~Q(legajosderivaciones__estado="Aceptada"),
+                    filter=~Q(legajosderivaciones__estado= EstadoDerivacion.objects.filter(estado="Aceptada").first()),
                     distinct=True,
                 ),
                 adolescente_riesgo=Count(
@@ -74,7 +74,7 @@ def obtener_legajos_counts():
                 ),
                 adolescente_sin_derivacion_aceptada=Count(
                     "id",
-                    filter=~Q(legajosderivaciones__estado="Aceptada"),
+                    filter=~Q(legajosderivaciones__estado=EstadoDerivacion.objects.filter(estado="Aceptada").first()),
                     distinct=True,
                 ),
             )
@@ -177,7 +177,7 @@ def calcular_porcentaje_respecto_a_poblacion(cantidad_legajos):
 
 
 def deriv_pendientes():
-    return LegajosDerivaciones.objects.filter(estado="Pendiente").distinct().count()
+    return LegajosDerivaciones.objects.filter(estado = EstadoDerivacion.objects.filter(estado="Pendiente").first()).distinct().count()
 
 
 def contar_legajos_embarazados():
@@ -188,8 +188,7 @@ def contar_legajos_embarazados():
 def contar_embarazos_sin_derivacion_aceptada():
     legajos_con_alerta_embarazo = obtener_legajos_con_alerta_embarazo()
     return legajos_con_alerta_embarazo.exclude(
-        legajosderivaciones__estado="Aceptada"
-    ).count()
+        legajosderivaciones__estado=EstadoDerivacion.objects.filter(estado="Aceptada").first()).count()
 
 
 def contar_embarazos_en_riesgo():
