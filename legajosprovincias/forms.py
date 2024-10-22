b6c2b3529e3b63f0722a0033de309c556d6ca00c
Nenu <nehuennavarro@gmail.com>
2024-10-22 12:34:59 -0300

    modelos,urls,views,templates

---
+++ legajosprovincias/forms.py
@@ -0,0 +1,71 @@
+from datetime import date
+
+from django import forms
+from django.core.validators import MaxValueValidator, MinValueValidator
+
+from usuarios.validators import MaxSizeFileValidator
+
+from .models import (
+    LegajosProvincias,
+    Presupuesto,
+    HistorialPresupuesto,
+    Proyectos,
+    DocumentacionArchivos,
+)
+
+class LegajosProvinciasForm(forms.ModelForm):
+    """Formulario para las provincias de los legajos."""
+    class Meta:
+        model = LegajosProvincias
+        fields = "__all__"
+        widgets = {
+            "agroindustria_alimentos": forms.CheckboxInput(),
+            "agroindustria_ganaderia": forms.CheckboxInput(),
+            "agroindustria_agricultura_familiar": forms.CheckboxInput(),
+            "agroindustria_pesca": forms.CheckboxInput(),
+            "agroindustria_forestal": forms.CheckboxInput(),
+            "agroindustria_manufactura": forms.CheckboxInput(),
+            "oficios_carpinteria": forms.CheckboxInput(),
+            "oficios_electicista": forms.CheckboxInput(),
+            "oficios_mecanica": forms.CheckboxInput(),
+            "oficios_herreria": forms.CheckboxInput(),
+            "oficios_jardineria": forms.CheckboxInput(),
+            "oficios_gastronomico": forms.CheckboxInput(),
+            "oficios_logistica": forms.CheckboxInput(),
+            "oficios_textil": forms.CheckboxInput(),
+            "oficios_soldador": forms.CheckboxInput(),
+            "oficios_plomeria": forms.CheckboxInput(),
+            "oficios_albanileria": forms.CheckboxInput(),
+            "oficios_panaderia": forms.CheckboxInput(),
+            "oficios_auxiliar_salud": forms.CheckboxInput(),
+            "economia_circular_ener_renovable": forms.CheckboxInput(),
+            "economia_circular_reciclaje": forms.CheckboxInput(),
+            "tecnologia_software_soporte_tecnico": forms.CheckboxInput(),
+        }
+
+class PresupuestoForm(forms.ModelForm):
+    """Formulario para el presupuesto."""
+    class Meta:
+        model = Presupuesto
+        fields = "__all__"
+
+class PresupuestoGastadoForm(forms.ModelForm):
+    """Formulario para el presupuesto gastado."""
+    class Meta:
+        model = HistorialPresupuesto
+        fields = "__all__"
+
+class ProyectosForm(forms.ModelForm):
+    """Formulario para los proyectos."""
+    class Meta:
+        model = Proyectos
+        fields = "__all__"
+
+class DocumentacionArchivosForm(forms.ModelForm):
+    """Formulario para la documentacion de archivos."""
+    class Meta:
+        model = DocumentacionArchivos
+        fields = "__all__"
+        widgets = {
+            "archivo": forms.FileInput(attrs={"accept": ".pdf, .doc, .docx, .xls, .xlsx, .jpg, .jpeg, .png, .gif, .zip, .rar, .7z, .tar.gz, .tar.bz2, .tar.xz, .tar"}),
+        }
--- legajosprovincias/models.py
+++ legajosprovincias/models.py
@@ -1,3 +1,135 @@
-from django.db import models
+from datetime import date  # pylint: disable=too-many-lines
 
-# Create your models here.
+from django.core.exceptions import ValidationError
+from django.core.validators import MaxValueValidator, MinValueValidator
+from django.db import models
+from django.urls import reverse
+from multiselectfield import MultiSelectField
+from usuarios.models import User, Usuarios
+
+class LegajosProvincias(models.Model):
+    """Modelo para las provincias de los legajos."""
+    nombre = models.CharField(max_length=50)
+    abreviatura = models.CharField(max_length=10)
+    poblacion = models.PositiveIntegerField()
+    porcentaje_pobreza = models.FloatField(
+        validators=[MinValueValidator(0), MaxValueValidator(100)]
+    )
+    poblacion_economicamente_activa = models.PositiveIntegerField()
+    porcentaje_empleo = models.FloatField(
+        validators=[MinValueValidator(0), MaxValueValidator(100)]
+    )
+    porcentaje_educacion_incompleta_obligatoria = models.FloatField(
+        validators=[MinValueValidator(0), MaxValueValidator(100)]
+    )
+    porcentaje_primaria_incompleta = models.FloatField(
+        validators=[MinValueValidator(0), MaxValueValidator(100)]
+    )
+    porcentaje_secundaria_incompleta = models.FloatField(
+        validators=[MinValueValidator(0), MaxValueValidator(100)]
+    )
+    cantidad_de_beneficiarios_paz = models.PositiveIntegerField()
+    porcentaje_paz = models.FloatField(
+        validators=[MinValueValidator(0), MaxValueValidator(100)]
+    )
+    cantidad_de_expendientes = models.PositiveIntegerField()
+    agroindustria_alimentos = models.BooleanField()
+    agroindustria_ganaderia = models.BooleanField()
+    agroindustria_agricultura_familiar = models.BooleanField()
+    agroindustria_pesca = models.BooleanField()
+    agroindustria_forestal = models.BooleanField()
+    agroindustria_manufactura = models.BooleanField()
+
+    oficios_carpinteria = models.BooleanField()
+    oficios_electicista = models.BooleanField()
+    oficios_mecanica = models.BooleanField()
+    oficios_herreria = models.BooleanField()
+    oficios_jardineria = models.BooleanField()
+    oficios_gastronomico = models.BooleanField()
+    oficios_logistica = models.BooleanField()
+    oficios_textil = models.BooleanField()
+    oficios_soldador = models.BooleanField()
+    oficios_plomeria = models.BooleanField()
+    oficios_albanileria = models.BooleanField()
+    oficios_panaderia = models.BooleanField()
+    oficios_auxiliar_salud = models.BooleanField()
+
+    economia_circular_ener_renovable = models.BooleanField()
+    economia_circular_reciclaje = models.BooleanField()
+
+    tecnologia_software_soporte_tecnico = models.BooleanField()
+    tecnologia_software_infraestructura_informatica = models.BooleanField()
+    tecnologia_software_desarollo_software = models.BooleanField()
+    tecnologia_software_instalaciones_tecnicas = models.BooleanField()
+    tecnologia_software_infraestructura_digital = models.BooleanField()
+
+    servicios_lamapara = models.BooleanField()
+    servicios_transporte = models.BooleanField()
+    servicios_cuidados = models.BooleanField()
+    servicios_salud = models.BooleanField()
+    servicios_educacion = models.BooleanField()
+
+    energia = models.BooleanField()
+    mineria = models.BooleanField()
+    industria_petroquimica = models.BooleanField()
+
+    class Meta:
+        verbose_name = "Provincia"
+        verbose_name_plural = "Provincias"
+
+    def __str__(self):
+        return f"{self.nombre}"
+    
+class Presupuesto (models.Model):
+    """Modelo para los presupuestos de los legajos."""
+    provincia = models.ForeignKey(LegajosProvincias, on_delete=models.CASCADE)
+    anio = models.PositiveIntegerField()
+    presupuesto_total = models.PositiveIntegerField()
+    presupuesto_linea_socio_productivo = models.PositiveIntegerField()
+    presupuesto_linea_formativo = models.PositiveIntegerField()
+    class Meta:
+        verbose_name = "Presupuesto"
+        verbose_name_plural = "Presupuestos"
+
+    def __str__(self):
+        return f"{self.provincia} - {self.anio}"
+
+class Proyectos (models.Model):
+    """Modelo para los proyectos de los legajos."""
+    provincia = models.ForeignKey(LegajosProvincias, on_delete=models.CASCADE)
+    proyecto = models.CharField(max_length=50)
+    presupuesto = models.ForeignKey(Presupuesto, on_delete=models.CASCADE)
+    class Meta:
+        verbose_name = "Proyecto"
+        verbose_name_plural = "Proyectos"
+
+    def __str__(self):
+        return f"{self.provincia} - {self.proyecto}"
+    
+class DocumentacionArchivos (models.Model):
+    """Modelo para los archivos de documentación de los legajos."""
+    provincia = models.ForeignKey(LegajosProvincias, on_delete=models.CASCADE)
+    proyecto = models.ForeignKey(Proyectos, on_delete=models.CASCADE)
+    fecha_creacion = models.DateField()
+    fecha_modificacion = models.DateField()
+    archivo = models.FileField(upload_to="documentacion/")
+    class Meta:
+        verbose_name = "Archivo"
+        verbose_name_plural = "Archivos"
+
+    def __str__(self):
+        return f"{self.provincia} - {self.archivo}"
+
+class HistorialPresupuesto (models.Model):
+    """Modelo para el historial de los presupuestos de los legajos."""
+    provincia = models.ForeignKey(LegajosProvincias, on_delete=models.CASCADE)
+    anio = models.PositiveIntegerField()
+    presupuesto_total = models.PositiveIntegerField()
+    presupuesto_linea_socio_productivo = models.PositiveIntegerField()
+    presupuesto_linea_formativo = models.PositiveIntegerField()
+    class Meta:
+        verbose_name = "Historial de Presupuesto"
+        verbose_name_plural = "Historial de Presupuestos"
+
+    def __str__(self):
+        return f"{self.provincia} - {self.anio}"
