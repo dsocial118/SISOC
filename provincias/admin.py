be9bd8d347a55b08cd65d2e0521fc4fd03d53f59
juanikitro <jpkitro@gmail.com>
2024-10-31 15:13:39 -0300

    #162 add bloque 5

--- provincias/admin.py
+++ provincias/admin.py
@@ -30,6 +30,17 @@ from provincias.models import (
     CanalesVentas,
     DestinoMaterialesRecuperados,
     ModalidadCicloProductivo,
+    Proyecto,
+    AnexoSocioProductivo,
+    PersoneriaOrganizacion,
+    PersoneriaPersona,
+    LineaDeAccion,
+    Presupuesto,
+    DestinatarioDirecto,
+    DiagnosticoOrganizacion,
+    DiagnosticoPersona,
+    Observacion,
+    AnexoFormacion,
 )
 
 # Registrar todos los modelos en el admin de Django
@@ -63,3 +74,14 @@ admin.site.register(MedioPlanificacion)
 admin.site.register(CanalesVentas)
 admin.site.register(DestinoMaterialesRecuperados)
 admin.site.register(ModalidadCicloProductivo)
+admin.site.register(Proyecto)
+admin.site.register(AnexoSocioProductivo)
+admin.site.register(PersoneriaOrganizacion)
+admin.site.register(PersoneriaPersona)
+admin.site.register(LineaDeAccion)
+admin.site.register(Presupuesto)
+admin.site.register(DestinatarioDirecto)
+admin.site.register(DiagnosticoOrganizacion)
+admin.site.register(DiagnosticoPersona)
+admin.site.register(Observacion)
+admin.site.register(AnexoFormacion)
