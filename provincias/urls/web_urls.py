1c67ff830a1b29ba1e6a6d5b80097656c9300df3
MariaNavarro90 <navarromariavictoria8@gmail.com>
2024-11-05 10:04:26 -0300

    Agregado patrones de URL para las vistas del Proyecto

--- provincias/urls/web_urls.py
+++ provincias/urls/web_urls.py
@@ -1,4 +1,9 @@
-from django.contrib.auth.decorators import login_required
 from django.urls import path
+from provincias.views.web_views import ProyectoCreateView, ProyectoListView, ProyectoUpdateView, ProyectoDeleteView
 
-urlpatterns = []
+urlpatterns = [
+    path('proyectos/', ProyectoListView.as_view(), name='proyecto_list'),
+    path('proyectos/create/', ProyectoCreateView.as_view(), name='proyecto_create'),
+    path('proyectos/<int:pk>/update/', ProyectoUpdateView.as_view(), name='proyecto_update'),
+    path('proyectos/<int:pk>/delete/', ProyectoDeleteView.as_view(), name='proyecto_delete'),
+]
