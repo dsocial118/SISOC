c9c6ae8c2a34b08d45a41d4793b14510115a8c6f
MariaNavarro90 <navarromariavictoria8@gmail.com>
2024-12-17 15:54:55 -0300

    Agregado model ValorComida, creadas las señales para el dashboard

--- dashboard/signals.py
+++ dashboard/signals.py
@@ -1,25 +1,16 @@
 from django.db.models.signals import post_delete, post_save
 from django.dispatch import receiver
-
 from configuraciones.models import Alertas
 from dashboard.models import Dashboard
+from comedores.models import Comedor, Relevamiento, Prestacion, ValorComida
 from dashboard.utils import (
-    contar_adolescente_riesgo,
-    contar_adolescente_sin_derivacion_aceptada,
-    contar_bb_riesgo,
-    contar_bb_sin_derivacion_aceptada,
-    contar_embarazos_en_riesgo,
-    contar_embarazos_sin_derivacion_aceptada,
-    contar_legajos,
-    contar_legajos_con_alarmas_activas,
-    contar_legajos_con_planes_sociales,
-    contar_legajos_embarazados,
-    contar_legajos_entre_0_y_18_anios,
-    contar_legajos_entre_0_y_40_dias,
-    deriv_pendientes,
+    contar_comedores_activos,
+    contar_relevamientos_activos,
+    contar_beneficiarios,
+    calcular_presupuesto_desayuno,
+    calcular_presupuesto_merienda,
+    calcular_presupuesto_comida,
 )
-from legajos.models import LegajoAlertas, Legajos, LegajosDerivaciones
-
 from .utils import table_exists
 
 
@@ -36,81 +27,52 @@ def update_dashboard_key(llave, cantidad):
     )
     return dashboard
 
-
-def update_dashboard_legajos(sender, instance, **kwargs):
-    """
-    Ejecuta todas las operaciones para que el Dashboard tenga informacion en tiempo real.
-    Guarda el resultado de dichas operaciones en el modelo Dashboard.
-    """
-    update_dashboard_key(
-        "cantidad_legajos_con_alarmas_activas", contar_legajos_con_alarmas_activas()
-    )
-    update_dashboard_key(
-        "cantidad_legajos_con_planes_sociales", contar_legajos_con_planes_sociales()
-    )
-    cantidad_total_legajos, cantidad_legajos_activos = contar_legajos()
-    update_dashboard_key("cantidad_total_legajos", cantidad_total_legajos)
-    update_dashboard_key("cantidad_legajos_activos", cantidad_legajos_activos)
-    update_dashboard_key(
-        "cantidad_legajos_entre_0_y_18_anios", contar_legajos_entre_0_y_18_anios()
-    )
-    update_dashboard_key("cantidad_legajos_40_dias", contar_legajos_entre_0_y_40_dias())
-    update_dashboard_key("cantidad_legajos_embarazados", contar_legajos_embarazados())
-    update_dashboard_key("cantidad_bb_riesgo", contar_bb_riesgo())
-    update_dashboard_key("cantidad_adolescente_riesgo", contar_adolescente_riesgo())
-    update_dashboard_key(
-        "cantidad_bb_sin_derivacion_aceptada", contar_bb_sin_derivacion_aceptada()
-    )
-    update_dashboard_key(
-        "cantidad_adolescente_sin_derivacion_aceptada",
-        contar_adolescente_sin_derivacion_aceptada(),
-    )
-    update_dashboard_key(
-        "embarazos_sin_derivacion_aceptada", contar_embarazos_sin_derivacion_aceptada()
-    )
-    update_dashboard_key("cantidad_embarazos_en_riesgo", contar_embarazos_en_riesgo())
-    update_dashboard_key("cantidad_dv_pendientes", deriv_pendientes())
+def update_dashboard_comedores(sender, instance, **kwargs):
+    update_dashboard_key("cantidad_comedores_activos", contar_comedores_activos())
+    update_dashboard_key("cantidad_relevamientos_activos", contar_relevamientos_activos())
+    #update_dashboard_key("cantidad_beneficiarios", contar_beneficiarios())
+    update_dashboard_key("presupuesto_desayuno", calcular_presupuesto_desayuno())
+    update_dashboard_key("presupuesto_merienda", calcular_presupuesto_merienda())
+    update_dashboard_key("presupuesto_comida", calcular_presupuesto_comida())
 
 
 def register_signals():
     if (
-        table_exists("legajos_legajos")
-        and table_exists("legajos_legajoalertas")
-        and table_exists("legajos_legajosderivaciones")
-        and table_exists("configuraciones_alertas")
+        table_exists("comedores_comedor")
+        and table_exists("comedores_relevamiento")
+        and table_exists("comedores_prestacion")
+        and table_exists("comedores_valorcomida")
     ):
+        @receiver(post_save, sender=Comedor)
+        def trigger_update_comedor(sender, instance, **kwargs):
+            return update_dashboard_comedores(sender, instance, **kwargs)
 
-        @receiver(post_save, sender=Legajos)
-        def trigger_update_legajo(sender, instance, **kwargs):
-            return update_dashboard_legajos(sender, instance, **kwargs)
+        @receiver(post_save, sender=Relevamiento)
+        def trigger_update_relevamiento(sender, instance, **kwargs):
+            return update_dashboard_comedores(sender, instance, **kwargs)
 
-        @receiver(post_save, sender=LegajoAlertas)
-        def trigger_update_legajoalertas(sender, instance, **kwargs):
-            return update_dashboard_legajos(sender, instance, **kwargs)
+        @receiver(post_save, sender=Prestacion)
+        def trigger_update_prestacion(sender, instance, **kwargs):
+            return update_dashboard_comedores(sender, instance, **kwargs)
 
-        @receiver(post_save, sender=LegajosDerivaciones)
-        def trigger_update_legajoderivaciones(sender, instance, **kwargs):
-            return update_dashboard_legajos(sender, instance, **kwargs)
+        @receiver(post_save, sender=ValorComida)
+        def trigger_update_valorcomida(sender, instance, **kwargs):
+            return update_dashboard_comedores(sender, instance, **kwargs)
 
-        @receiver(post_save, sender=Alertas)
-        def trigger_update_alertas(sender, instance, **kwargs):
-            return update_dashboard_legajos(sender, instance, **kwargs)
+        @receiver(post_delete, sender=Comedor)
+        def trigger_update_delete_comedor(sender, instance, **kwargs):
+            return update_dashboard_comedores(sender, instance, **kwargs)
 
-        @receiver(post_delete, sender=Legajos)
-        def trigger_update_delete_legajo(sender, instance, **kwargs):
-            return update_dashboard_legajos(sender, instance, **kwargs)
+        @receiver(post_delete, sender=Relevamiento)
+        def trigger_update_delete_relevamiento(sender, instance, **kwargs):
+            return update_dashboard_comedores(sender, instance, **kwargs)
 
-        @receiver(post_delete, sender=LegajoAlertas)
-        def trigger_update_delete_legajoalertas(sender, instance, **kwargs):
-            return update_dashboard_legajos(sender, instance, **kwargs)
+        @receiver(post_delete, sender=Prestacion)
+        def trigger_update_delete_prestacion(sender, instance, **kwargs):
+            return update_dashboard_comedores(sender, instance, **kwargs)
 
-        @receiver(post_delete, sender=LegajosDerivaciones)
-        def trigger_update_delete_legajoderivaciones(sender, instance, **kwargs):
-            return update_dashboard_legajos(sender, instance, **kwargs)
-
-        @receiver(post_delete, sender=Alertas)
-        def trigger_update_delete_alertas(sender, instance, **kwargs):
-            return update_dashboard_legajos(sender, instance, **kwargs)
-
+        @receiver(post_delete, sender=ValorComida)
+        def trigger_update_delete_valorcomida(sender, instance, **kwargs):
+            return update_dashboard_comedores(sender, instance, **kwargs)
 
 register_signals()
