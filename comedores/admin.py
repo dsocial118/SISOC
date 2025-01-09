0f2d5a11f76023163c370ecbfe6c98eb022706e8
juanikitro <jpkitro@gmail.com>
2025-01-09 17:13:42 -0300

    Añadi fixtures y admins para las intervenciones

--- comedores/admin.py
+++ comedores/admin.py
@@ -18,172 +18,32 @@ from comedores.models import (
     TipoRecurso,
     TipoGestionQuejas,
     ValorComida,
+    Intervencion,
+    SubIntervencion,
+    TipoIntervencion,
+    EstadosIntervencion,
+    Nomina,
 )
 
-
-@admin.register(TipoModalidadPrestacion)
-class TipoModalidadPrestacionAdmin(admin.ModelAdmin):
-    list_display = ("nombre",)
-
-    search_fields = ["nombre"]
-    ordering = ["nombre"]
-
-
-@admin.register(TipoEspacio)
-class TipoEspacioAdmin(admin.ModelAdmin):
-    list_display = ("nombre",)
-
-    search_fields = ["nombre"]
-    ordering = ["nombre"]
-
-
-@admin.register(TipoCombustible)
-class TipoCombustibleAdmin(admin.ModelAdmin):
-    list_display = ("nombre",)
-
-    search_fields = ["nombre"]
-    ordering = ["nombre"]
-
-
-@admin.register(TipoAgua)
-class TipoAguaAdmin(admin.ModelAdmin):
-    list_display = ("nombre",)
-
-    search_fields = ["nombre"]
-    ordering = ["nombre"]
-
-
-@admin.register(TipoDesague)
-class TipoDesagueAdmin(admin.ModelAdmin):
-    list_display = ("nombre",)
-
-    search_fields = ["nombre"]
-    ordering = ["nombre"]
-
-
-@admin.register(FrecuenciaLimpieza)
-class FrecuenciaLimpiezaAdmin(admin.ModelAdmin):
-    list_display = ("nombre",)
-
-    search_fields = ["nombre"]
-    ordering = ["nombre"]
-
-
-@admin.register(CantidadColaboradores)
-class CantidadColaboradoresAdmin(admin.ModelAdmin):
-    list_display = ("nombre",)
-
-    search_fields = ["nombre"]
-    ordering = ["nombre"]
-
-
-@admin.register(FrecuenciaRecepcionRecursos)
-class FrecuenciaRecepcionRecursosAdmin(admin.ModelAdmin):
-    list_display = ("nombre",)
-
-    search_fields = ["nombre"]
-    ordering = ["nombre"]
-
-
-@admin.register(TipoGestionQuejas)
-class TipoGestionQuejasAdmin(admin.ModelAdmin):
-    list_display = ("nombre",)
-
-    search_fields = ["nombre"]
-    ordering = ["nombre"]
-
-
-@admin.register(TipoRecurso)
-class TipoRecursoAdmin(admin.ModelAdmin):
-    list_display = ("nombre",)
-
-    search_fields = ["nombre"]
-    ordering = ["nombre"]
-
-
-@admin.register(FuenteRecursos)
-class FuenteRecursosAdmin(admin.ModelAdmin):
-    list_display = (
-        "recibe_donaciones_particulares",
-        "recibe_estado_nacional",
-        "recibe_estado_provincial",
-        "recibe_estado_municipal",
-        "recibe_otros",
-    )
-
-
-@admin.register(FuenteCompras)
-class FuenteComprasAdmin(admin.ModelAdmin):
-    list_display = (
-        "almacen_cercano",
-        "verduleria",
-        "granja",
-        "carniceria",
-        "pescaderia",
-        "supermercado",
-        "mercado_central",
-        "ferias_comunales",
-        "mayoristas",
-        "otro",
-    )
-
-
-@admin.register(Prestacion)
-class PrestacionAdmin(admin.ModelAdmin):
-    list_display = ["relevamiento"]
-    search_fields = ["relevamiento"]
-    ordering = ["relevamiento"]
-
-
-@admin.register(Comedor)
-class ComedorAdmin(admin.ModelAdmin):
-    list_display = (
-        "nombre",
-        "calle",
-        "numero",
-        "provincia",
-        "municipio",
-        "localidad",
-    )
-
-    search_fields = ["nombre", "provincia", "municipio", "localidad"]
-    ordering = ["nombre"]
-
-
-@admin.register(Relevamiento)
-class RelevamientoAdmin(admin.ModelAdmin):
-    list_display = (
-        "comedor",
-        "fecha_visita",
-    )
-    search_fields = [
-        "comedor",
-        "fecha_visita",
-    ]
-    ordering = ["comedor"]
-
-
-@admin.register(Observacion)
-class ObservacionAdmin(admin.ModelAdmin):
-    list_display = (
-        "comedor",
-        "fecha_visita",
-    )
-    search_fields = [
-        "comedor",
-        "fecha_visita",
-    ]
-    ordering = ["comedor"]
-
-
-@admin.register(ValorComida)
-class ValorComidaAdmin(admin.ModelAdmin):
-    list_display = (
-        "tipo",
-        "valor",
-    )
-    search_fields = [
-        "tipo",
-        "valor",
-    ]
-    ordering = ["tipo"]
+admin.site.register(TipoModalidadPrestacion)
+admin.site.register(TipoEspacio)
+admin.site.register(TipoCombustible)
+admin.site.register(TipoAgua)
+admin.site.register(TipoDesague)
+admin.site.register(FrecuenciaLimpieza)
+admin.site.register(CantidadColaboradores)
+admin.site.register(FrecuenciaRecepcionRecursos)
+admin.site.register(TipoGestionQuejas)
+admin.site.register(TipoRecurso)
+admin.site.register(FuenteRecursos)
+admin.site.register(FuenteCompras)
+admin.site.register(Prestacion)
+admin.site.register(Comedor)
+admin.site.register(Relevamiento)
+admin.site.register(Observacion)
+admin.site.register(ValorComida)
+admin.site.register(Intervencion)
+admin.site.register(SubIntervencion)
+admin.site.register(TipoIntervencion)
+admin.site.register(EstadosIntervencion)
+admin.site.register(Nomina)
