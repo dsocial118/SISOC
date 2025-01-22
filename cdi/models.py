204103172f1f51133303dfb06285e1b3c1243d88
MKDir <farinamatias00@gmail.com>
2025-01-22 12:32:43 -0300

    CDI

--- cdi/models.py
+++ cdi/models.py
@@ -10,38 +10,11 @@ from comedores.models import Referente
 
 
 
-class Organizacion(models.Model):
-    nombre = models.CharField(max_length=255)
-    cuit = models.CharField(max_length=13, unique=True)
-    # 
-    provincia = models.ForeignKey(to=Provincia, on_delete=models.PROTECT, null=True)
-    municipio = models.ForeignKey(
-        to=Municipio, on_delete=models.PROTECT, null=True, blank=True
-    )
-    localidad = models.ForeignKey(
-        to=Localidad, on_delete=models.PROTECT, null=True, blank=True
-    )
-    partido = models.CharField(max_length=255, null=True, blank=True)
-    barrio = models.CharField(max_length=255, null=True, blank=True)
-    codigo_postal = models.IntegerField(
-        validators=[
-            MinValueValidator(1000),
-            MaxValueValidator(999999),
-        ],  # Entre 4 a 6 digitos
-        blank=True,
-        null=True,
-    )
-    # 
-    direccion = models.TextField()
-    telefono = models.CharField(max_length=20)
-    email = models.EmailField()
-    referente = models.ForeignKey(
-        to=Referente, on_delete=models.SET_NULL, null=True, blank=True
-    )
+
 
 class CentroDesarrolloInfantil(models.Model):
     nombre = models.CharField(max_length=255)
-    organizacion = models.ForeignKey(Organizacion, on_delete=models.CASCADE)
+    # organizacion = models.ForeignKey(Organizacion, on_delete=models.CASCADE)
     comienzo = models.IntegerField(
         validators=[
             MinValueValidator(1900),
