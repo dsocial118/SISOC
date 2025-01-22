8f3b4109c6d3bfa762b65b1e2d1d40d7ea194c7a
juanikitro <jpkitro@gmail.com>
2025-01-22 14:46:25 -0300

    Implemente sincronizacion en todos los cambios y todos los campos de relevamiento hacia GESTIONAR

--- comedores/signals.py
+++ comedores/signals.py
@@ -14,8 +14,7 @@ def send_comedor_to_gestionar(sender, instance, created, **kwargs):
 
 @receiver(post_save, sender=Relevamiento)
 def send_relevamiento_to_gestionar(sender, instance, created, **kwargs):
-    if created:
-        RelevamientoService.send_to_gestionar(instance)
+    RelevamientoService.send_to_gestionar(instance)
 
 
 @receiver(post_save, sender=Observacion)
