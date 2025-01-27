43149db33bac55ef851d37524b1632981e6020d4
juanikitro <jpkitro@gmail.com>
2025-01-27 13:18:44 -0300

    Implemente signals pre-delete para sincronizar el borrado de comedores y relevamiento con GESTIONAR

--- comedores/signals.py
+++ comedores/signals.py
@@ -1,4 +1,4 @@
-from django.db.models.signals import post_save
+from django.db.models.signals import post_save, pre_delete
 from django.dispatch import receiver
 from comedores.models.comedor import Observacion, Referente, Comedor
 from comedores.models.relevamiento import Relevamiento
@@ -13,10 +13,20 @@ def send_comedor_to_gestionar(sender, instance, created, **kwargs):
         ComedorService.send_to_gestionar(instance)
 
 
+@receiver(pre_delete, sender=Comedor)
+def remove_comedor_to_gestionar(sender, instance, using, **kwargs):
+    ComedorService.remove_to_gestionar(instance)
+
+
 @receiver(post_save, sender=Relevamiento)
 def send_relevamiento_to_gestionar(sender, instance, created, **kwargs):
     if created:
         RelevamientoService.send_to_gestionar(instance)
+
+
+@receiver(pre_delete, sender=Relevamiento)
+def remove_relevamiento_to_gestionar(sender, instance, using, **kwargs):
+    RelevamientoService.remove_to_gestionar(instance)
 
 
 @receiver(post_save, sender=Observacion)
