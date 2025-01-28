7371f73f780abcc356eafe3258898769ec5a5ca5
dsocial118 <167777359+dsocial118@users.noreply.github.com>
2025-01-28 14:51:19 -0300

    Fix sincronizar cambios en comedores

--- comedores/signals.py
+++ comedores/signals.py
@@ -1,4 +1,4 @@
-from django.db.models.signals import post_save, pre_delete
+from django.db.models.signals import post_save, pre_delete, pre_save
 from django.dispatch import receiver
 from comedores.models.comedor import Observacion, Referente, Comedor
 from comedores.models.relevamiento import Relevamiento
@@ -9,8 +9,19 @@ from comedores.services.comedor_service import ComedorService
 
 @receiver(post_save, sender=Comedor)
 def send_comedor_to_gestionar(sender, instance, created, update_fields, **kwargs):
-    if created or update_fields:
+    if created:
         ComedorService.send_to_gestionar(instance)
+
+
+@receiver(pre_save, sender=Comedor)
+def update_comedor_in_gestionar(sender, instance, **kwargs):
+    if instance.pk:  # Solo para updates
+        previous_instance = sender.objects.get(pk=instance.pk)
+        for field in instance._meta.fields:
+            field_name = field.name
+            if getattr(instance, field_name) != getattr(previous_instance, field_name):
+                ComedorService.send_to_gestionar(instance)
+                break
 
 
 @receiver(pre_delete, sender=Comedor)
@@ -19,8 +30,8 @@ def remove_comedor_to_gestionar(sender, instance, using, **kwargs):
 
 
 @receiver(post_save, sender=Relevamiento)
-def send_relevamiento_to_gestionar(sender, instance, created, update_fields, **kwargs):
-    if created or update_fields:
+def send_relevamiento_to_gestionar(sender, instance, created, **kwargs):
+    if created:
         RelevamientoService.send_to_gestionar(instance)
 
 
