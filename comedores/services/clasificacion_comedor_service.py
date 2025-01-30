b57bef1e73cb49de5f42f1238d56a8bc7a7d5b96
Nenu <nehuennavarro@gmail.com>
2025-01-30 14:38:55 -0300

    cambios

--- comedores/services/clasificacion_comedor_service.py
+++ comedores/services/clasificacion_comedor_service.py
@@ -23,7 +23,7 @@ class ClasificacionComedorService:
         clasificacionFinal = ClasificacionComedorService.get_clasificacion(calculoPuntuacion)
         ClasificacionComedor.objects.create(
             relevamiento=relevamiento,
-            clasificacion=clasificacionFinal,
+            categoria=clasificacionFinal,
             puntuacionTotal=calculoPuntuacion,
             comedor=relevamiento.comedor
         )
@@ -34,35 +34,171 @@ class ClasificacionComedorService:
         califiacion = CategoriaComedor.objects.filter(
             puntuacionMin__lte=puntacionTotal,
             puntuacionMax__gte=puntacionTotal
-        )
-        return califiacion[0].nombre if califiacion else None
+        ).first()
+        return califiacion if califiacion else None
     
     @staticmethod
     def get_puntuacion_total(relevamiento: Relevamiento):
         puntuacion = 0
         relevamiento_respuestas = []
-        relevamiento_respuestas.append(relevamiento.espacio.tipo_espacio_fisico)
-        relevamiento_respuestas.append(relevamiento.espacio.cocina.espacio_elaboracion_alimentos)
+
+        if relevamiento.espacio.tipo_espacio_fisico.nombre == 'Espacio alquilado':
+            puntuacion += 3
+        elif relevamiento.espacio.tipo_espacio_fisico.nombre == 'Espacio Prestado':
+            puntuacion += 2
+        elif relevamiento.espacio.tipo_espacio_fisico.nombre == 'Comunitario compartido':
+            puntuacion += 2
+        elif relevamiento.espacio.tipo_espacio_fisico.nombre == 'Casa de familia':
+            puntuacion += 3
+        elif relevamiento.espacio.espacio_fisico_otro is not "":
+            puntuacion += 3
+        else:
+            puntuacion += 0
+
+        
+        if relevamiento.espacio.cocina.espacio_elaboracion_alimentos is False:
+            puntuacion += 3
+        else:
+            puntuacion += 0
+
+        if relevamiento.espacio.cocina.almacenamiento_alimentos_secos is False:
+            puntuacion += 2
+        else:
+            puntuacion += 0
+
+        if relevamiento.espacio.cocina.heladera is False:
+            puntuacion += 3
+        else:
+            puntuacion += 0
+        
+        if relevamiento.espacio.cocina.recipiente_residuos_organicos is False:
+            puntuacion += 1
+        else:
+            puntuacion += 0
+        
+        if relevamiento.espacio.cocina.recipiente_residuos_reciclables is False:
+            puntuacion += 1
+        else:
+            puntuacion += 0
+        
+        if relevamiento.espacio.cocina.abastecimiento_combustible.filter(nombre='Leña').exists():
+            puntuacion += 3
+        elif relevamiento.espacio.cocina.abastecimiento_combustible.filter(nombre='Otro').exists():
+            puntuacion += 2
+        elif relevamiento.espacio.cocina.abastecimiento_combustible.filter(nombre='Gas envasado').exists():
+            puntuacion += 1
+        else:
+            puntuacion += 0
+
+        if relevamiento.espacio.cocina.abastecimiento_agua.nombre == 'Pozo':
+            puntuacion += 2
+        elif relevamiento.espacio.cocina.abastecimiento_agua_otro != "":
+            puntuacion += 3
+        else:
+            puntuacion += 0
+        
+        if relevamiento.espacio.cocina.instalacion_electrica is False:
+            puntuacion += 3
+        else:
+            puntuacion += 0
+        
+        if relevamiento.espacio.prestacion.espacio_equipado is False:
+            puntuacion += 2
+        else:  
+            puntuacion += 0
+        
+        if relevamiento.espacio.prestacion.tiene_ventilacion is False:
+            puntuacion += 3
+        else:
+            puntuacion += 0
+        
+        if relevamiento.espacio.prestacion.tiene_salida_emergencia is False:
+            puntuacion += 2
+        else:
+            puntuacion += 0
+        
+        if relevamiento.espacio.prestacion.salida_emergencia_senializada is False:
+            puntuacion += 1
+        else:
+            puntuacion += 0
+        
+        if relevamiento.espacio.prestacion.tiene_equipacion_incendio is False:
+            puntuacion += 3
+        else:
+            puntuacion += 0
+
+        if relevamiento.espacio.prestacion.tiene_botiquin is False:
+            puntuacion += 3
+        else:
+            puntuacion += 0
+        
+        if relevamiento.espacio.prestacion.tiene_buena_iluminacion is False:
+            puntuacion += 2
+        else:
+            puntuacion += 0
+        
+        if relevamiento.espacio.prestacion.tiene_sanitarios is False:
+            puntuacion += 3
+        else:
+            puntuacion += 0
+        
+        if relevamiento.espacio.prestacion.desague_hinodoro.nombre == 'Pozo ciego':
+            puntuacion += 2
+        elif relevamiento.espacio.prestacion.desague_hinodoro.nombre == 'Letrina':
+            puntuacion += 3
+        else:
+            puntuacion += 0
+
+        if relevamiento.anexo.tecnologia == "Computadora":
+            puntuacion += 2
+        elif relevamiento.anexo.tecnologia == "Celular":
+            puntuacion += 2
+        elif relevamiento.anexo.tecnologia == "Ninguno":
+            puntuacion += 3
+        else:
+            puntuacion += 0
+
+        if relevamiento.anexo.servicio_internet is False:
+            puntuacion += 1
+        else:
+            puntuacion += 0
+        
+        if relevamiento.anexo.acceso_comedor == "Calle de tierra":
+            puntuacion += 3
+        elif relevamiento.anexo.acceso_comedor == "Calle con mejorado":
+            puntuacion += 2
+        else:
+            puntuacion += 0
+        
+        if relevamiento.anexo.zona_inundable is False:
+            puntuacion += 3
+        else:
+            puntuacion += 0
+
+        relevamiento_respuestas.append(relevamiento.espacio.tipo_espacio_fisico.nombre)
         relevamiento_respuestas.append(relevamiento.espacio.cocina.almacenamiento_alimentos_secos)
         relevamiento_respuestas.append(relevamiento.espacio.cocina.heladera)
         relevamiento_respuestas.append(relevamiento.espacio.cocina.recipiente_residuos_organicos)
         relevamiento_respuestas.append(relevamiento.espacio.cocina.recipiente_residuos_reciclables)
+
         relevamiento_respuestas.append(relevamiento.espacio.cocina.abastecimiento_combustible)
-        relevamiento_respuestas.append(relevamiento.espacio.cocina.abastecimiento_agua)
+
+        relevamiento_respuestas.append(relevamiento.espacio.cocina.abastecimiento_agua.nombre)
         relevamiento_respuestas.append(relevamiento.espacio.cocina.abastecimiento_agua_otro)
         relevamiento_respuestas.append(relevamiento.espacio.cocina.instalacion_electrica)
-        relevamiento_respuestas.append(relevamiento.prestacion.espacio_equipado)
-        relevamiento_respuestas.append(relevamiento.prestacion.tiene_ventilacion)
-        relevamiento_respuestas.append(relevamiento.prestacion.tiene_salida_emergencia)
-        relevamiento_respuestas.append(relevamiento.prestacion.salida_emergencia_senializada)
-        relevamiento_respuestas.append(relevamiento.prestacion.tiene_equipacion_incendio)
-        relevamiento_respuestas.append(relevamiento.prestacion.tiene_botiquin)
-        relevamiento_respuestas.append(relevamiento.prestacion.tiene_buena_iluminacion)
-        relevamiento_respuestas.append(relevamiento.prestacion.tiene_sanitarios)
-        relevamiento_respuestas.append(relevamiento.prestacion.desague_hinodoro)
+        relevamiento_respuestas.append(relevamiento.espacio.prestacion.espacio_equipado)
+        relevamiento_respuestas.append(relevamiento.espacio.prestacion.tiene_ventilacion)
+        relevamiento_respuestas.append(relevamiento.espacio.prestacion.tiene_salida_emergencia)
+        relevamiento_respuestas.append(relevamiento.espacio.prestacion.salida_emergencia_senializada)
+        relevamiento_respuestas.append(relevamiento.espacio.prestacion.tiene_equipacion_incendio)
+        relevamiento_respuestas.append(relevamiento.espacio.prestacion.tiene_botiquin)
+        relevamiento_respuestas.append(relevamiento.espacio.prestacion.tiene_buena_iluminacion)
+        relevamiento_respuestas.append(relevamiento.espacio.prestacion.tiene_sanitarios)
+        relevamiento_respuestas.append(relevamiento.espacio.prestacion.desague_hinodoro.nombre)
         relevamiento_respuestas.append(relevamiento.anexo.tecnologia)
         relevamiento_respuestas.append(relevamiento.anexo.servicio_internet)
         relevamiento_respuestas.append(relevamiento.anexo.acceso_comedor)
         relevamiento_respuestas.append(relevamiento.anexo.zona_inundable)
+
         print(relevamiento_respuestas)
         return puntuacion
