34d5f7ee6122c4c9d215b6ce22a6b29a07acd3a8
MariaNavarro90 <navarromariavictoria8@gmail.com>
2025-03-20 14:46:37 -0300

    Eliminados Logs

--- static/custom/js/navigator.js
+++ static/custom/js/navigator.js
@@ -1,19 +1,15 @@
 function GetCurrentSection() {
   let path = window.location.pathname;
   let section = path.split("/")[1];
-  console.log("DebugLog section = " + section);
   return section;
 }
 
 function StoreCurrentSection() {
   let lastSection = sessionStorage.getItem("currentSection");
-  console.log("DebugLog previousSection = " + lastSection);
   let section = GetCurrentSection();
   if (lastSection == null || lastSection != section) {
     sessionStorage.setItem("currentSection", section);
-    console.log("DebugLog saved in session = " + sessionStorage.getItem("currentSection"));
     sessionStorage.removeItem("lastUrl");
-    console.log("DebugLog lastUrl removed from session");
   }
 }
 
@@ -24,7 +20,6 @@ document.addEventListener("DOMContentLoaded", function() {
     document.querySelectorAll('.link-handler').forEach(item => {
         item.addEventListener('click', event => {
           sessionStorage.setItem("lastUrl", window.location.href);
-          console.log("DebugLog lastUrl = " + sessionStorage.getItem("lastUrl"));
         })
       });
       
