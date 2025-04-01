d76819a0f26606c04a59e1b2d7cec976ab127db0
juanikitro <jpkitro@gmail.com>
2025-04-01 19:25:15 -0300

    Elimine modelos no usados y fixee los fitures

--- configuraciones/urls.py
+++ configuraciones/urls.py
@@ -1,8 +1,6 @@
 from django.urls import path
 
 from .views import (
-    load_asentamiento,
-    load_departamento,
     load_localidad,
     load_municipios,
 )
@@ -10,6 +8,4 @@ from .views import (
 urlpatterns = [
     path("ajax/load-municipios/", load_municipios, name="ajax_load_municipios"),
     path("ajax/load-localidades/", load_localidad, name="ajax_load_localidades"),
-    path("ajax/load-departamentos/", load_departamento, name="ajax_load_departamentos"),
-    path("ajax/load-asentamientos/", load_asentamiento, name="ajax_load_asentamientos"),
 ]
