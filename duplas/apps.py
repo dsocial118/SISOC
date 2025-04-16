5aa3fe06d1cc4d63c84ed4b4a72a29f9f277801b
Nenu <nehuennavarro@gmail.com>
2025-04-16 12:55:53 -0300

    cambios

--- duplas/apps.py
+++ duplas/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class DuplaConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
-    name = "dupla"
+    name = "duplas"
--- duplas/forms.py
+++ duplas/forms.py
@@ -1,18 +1,37 @@
 from django import forms
-from django.utils import timezone
-from django.utils import dateformat
+from django.contrib.auth.models import Group
 from duplas.models import Dupla
 
 
 class DuplaForm(forms.ModelForm):
+    
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.filtrar_campos_tecnico_aboagdo()
+
+    def filtrar_campos_tecnico_aboagdo(self):
+        # Filtrar usuarios que pertenezcan al grupo "Tecnico Comedor"
+        grupo_tecnico = Group.objects.filter(name="Tecnico Comedor").first()
+        if grupo_tecnico:
+            self.fields["tecnico"].queryset = grupo_tecnico.user_set.all()
+        else:
+            self.fields["tecnico"].queryset = self.fields["tecnico"].queryset.none()
+
+        # Filtrar usuarios que pertenezcan al grupo "Abogado Dupla"
+        grupo_abogado = Group.objects.filter(name="Abogado Dupla").first()
+        if grupo_abogado:
+            self.fields["abogado"].queryset = grupo_abogado.user_set.all()
+        else:
+            self.fields["abogado"].queryset = self.fields["abogado"].queryset.none()
+
+
     class Meta:
         model = Dupla
         fields = "__all__"
         widgets = {
             "tecnico": forms.CheckboxSelectMultiple(),
-            "fecha": forms.DateInput(
-                attrs={"type": "date", "class": "form-control"},
-            ),
+            "abogado": forms.Select(attrs={"class": "form-control"}),
         }
         labels = {
             "nombre": "Nombre",
