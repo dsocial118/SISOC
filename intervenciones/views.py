fb453cec669a3a330a177b05c7f8dbdc195dc715
MariaNavarro90 <navarromariavictoria8@gmail.com>
2025-04-29 15:35:19 -0300

    - Se añadieron los campos "destinatario", "forma_contacto" y "tiene_documentacion" al modelo de Intervención. - Se actualizó el formulario de Intervención para incluir nuevos campos con los tipos de entrada adecuados. - Se modificó el archivo intervencion_detail.html para mostrar los nuevos campos en la tabla de intervenciones. - Se mejoró el archivo intervencion_form.html para capturar datos adicionales en un formulario de varios pasos. - Se actualizó la lógica de filtrado en views.py para admitir nuevos campos en la lista de intervenciones.

--- intervenciones/views.py
+++ intervenciones/views.py
@@ -1,11 +1,13 @@
 # intervenciones/views.py
 from django.http import JsonResponse
 from django.shortcuts import redirect
+from django.urls import reverse
 from django.views.decorators.csrf import csrf_exempt
 from django.views.generic import CreateView, DeleteView, UpdateView, TemplateView
 from comedores.services.comedor_service import ComedorService
 from intervenciones.models.intervenciones import Intervencion, SubIntervencion
 from intervenciones.forms import IntervencionForm
+
 
 
 @csrf_exempt
@@ -28,10 +30,22 @@ class IntervencionDetail(TemplateView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
+        comedor = ComedorService.get_comedor(self.kwargs["pk"])
         intervenciones, cantidad_intervenciones = (
             ComedorService.detalle_de_intervencion(self.kwargs)
         )
-        comedor = ComedorService.get_comedor(self.kwargs["pk"])
+        intervenciones = Intervencion.objects.filter(comedor=comedor)
+        fecha = self.request.GET.get("fecha")
+        tipo_intervencion = self.request.GET.get("tipo_intervencion")
+        destinatario = self.request.GET.get("destinatario")
+
+        if fecha:
+            intervenciones = intervenciones.filter(fecha__date=fecha)
+        if tipo_intervencion:
+            intervenciones = intervenciones.filter(tipo_intervencion_id=tipo_intervencion)
+        if destinatario:
+            intervenciones = intervenciones.filter(destinatario__icontains=destinatario)
+
         context["intervenciones"] = intervenciones
         context["object"] = comedor
         context["cantidad_intervenciones"] = cantidad_intervenciones
@@ -44,14 +58,16 @@ class IntervencionCreateView(CreateView):
     form_class = IntervencionForm
 
     def form_valid(self, form):
-        pk = self.kwargs["pk"]
+        # Asociar la intervención con el comedor correspondiente
+        form.instance.comedor_id = self.kwargs["pk"]
         form.save()
-        return redirect("intervencion_ver", pk=pk)
+        # Redirigir al detalle de las intervenciones del comedor
+        return redirect(reverse("comedor_intervencion_ver", kwargs={"pk": self.kwargs["pk"]}))
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
+        # Obtener el comedor asociado
         comedor = ComedorService.get_comedor(self.kwargs["pk"])
-        context["form"] = self.get_form()
         context["object"] = comedor
         return context
 
