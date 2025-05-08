6b345b9d3ca2dbc508e88d030df7e6eeb1db868e
Nenu <nehuennavarro@gmail.com>
2025-05-08 17:00:10 -0300

    cambios

--- duplas/forms.py
+++ duplas/forms.py
@@ -13,8 +13,12 @@ class DuplaForm(forms.ModelForm):
         grupo_tecnico = Group.objects.filter(name="Tecnico Comedor").first()
         if grupo_tecnico:
             # Excluir usuarios que ya están asignados a una dupla, excepto el actual
-            usuarios_asignados = Dupla.objects.exclude(pk=self.instance.pk).values_list("tecnico", flat=True)
-            self.fields["tecnico"].queryset = grupo_tecnico.user_set.exclude(id__in=usuarios_asignados)
+            usuarios_asignados = Dupla.objects.exclude(pk=self.instance.pk).values_list(
+                "tecnico", flat=True
+            )
+            self.fields["tecnico"].queryset = grupo_tecnico.user_set.exclude(
+                id__in=usuarios_asignados
+            )
         else:
             self.fields["tecnico"].queryset = self.fields["tecnico"].queryset.none()
 
