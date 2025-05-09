e9847a31c25adf7d0b7339e719eb31b2835ad34a
Nenu <nehuennavarro@gmail.com>
2025-05-09 17:06:46 -0300

    cambios

--- organizaciones/forms.py
+++ organizaciones/forms.py
@@ -5,15 +5,18 @@ from organizaciones.models import (
     Firmante,
 )
 
+
 class OrganizacionForm(forms.ModelForm):
     class Meta:
         model = Organizacion
         fields = "__all__"
 
+
 class FirmanteForm(forms.ModelForm):
     class Meta:
         model = Firmante
         fields = ["nombre", "rol"]
+
 
 FirmanteFormset = inlineformset_factory(
     Organizacion, Firmante, form=FirmanteForm, extra=1, can_delete=True
--- organizaciones/models.py
+++ organizaciones/models.py
@@ -16,29 +16,28 @@ class TipoOrganizacion(models.Model):
 
 class Firmante(models.Model):
     organizacion = models.ForeignKey(
-        "Organizacion",
-        on_delete=models.CASCADE,
-        related_name="firmantes"
+        "Organizacion", on_delete=models.CASCADE, related_name="firmantes"
     )
     nombre = models.CharField(max_length=255)
     rol = models.CharField(
         max_length=50,
         choices=[
-                ("firmante1", "Firmante 1"), 
-                ("firmante2", "Firmante 2"), 
-                ("firmante3", "Firmante 3"), 
-                ("obispo", "Obispo"),
-                ("apoderado1", "Apoderado 1"),
-                ("apoderado2", "Apoderado 2"),
-                ("presidente", "Presidente"),
-                ("secretario"," Secretario"),
-                ("tesorero", "Tesorero"),
-                ("aval", "Aval 1"),
-                ("aval2", "Aval 2"),
-                ("cuit_aval1", "CUIT Aval 1"),
-                ("cuit_aval2", "CUIT Aval 2"),
-                ]
+            ("firmante1", "Firmante 1"),
+            ("firmante2", "Firmante 2"),
+            ("firmante3", "Firmante 3"),
+            ("obispo", "Obispo"),
+            ("apoderado1", "Apoderado 1"),
+            ("apoderado2", "Apoderado 2"),
+            ("presidente", "Presidente"),
+            ("secretario", " Secretario"),
+            ("tesorero", "Tesorero"),
+            ("aval", "Aval 1"),
+            ("aval2", "Aval 2"),
+            ("cuit_aval1", "CUIT Aval 1"),
+            ("cuit_aval2", "CUIT Aval 2"),
+        ],
     )
+
 
 class Organizacion(models.Model):
     nombre = models.CharField(max_length=255)
