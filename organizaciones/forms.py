5282a5cadc529d143f6e03cddad8eb074f6ce4dc
juanikitro <jpkitro@gmail.com>
2025-05-10 03:05:13 -0400

    Añadi flexibilidad al modelo Firmante

--- organizaciones/forms.py
+++ organizaciones/forms.py
@@ -15,7 +15,15 @@ class OrganizacionForm(forms.ModelForm):
 class FirmanteForm(forms.ModelForm):
     class Meta:
         model = Firmante
-        fields = ["nombre", "rol"]
+        fields = ["nombre", "rol", "cuit"]
+
+    def clean(self):
+        cleaned_data = super().clean()
+        rol = cleaned_data.get("rol")
+        cuit = cleaned_data.get("cuit")
+
+        if rol == "aval" and not cuit:
+            self.add_error("cuit", "El CUIT es obligatorio para el rol Aval")
 
 
 FirmanteFormset = inlineformset_factory(
