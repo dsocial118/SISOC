40e1e72adec70bd1c619abd1a31c027630e3e368
MariaNavarro90 <navarromariavictoria8@gmail.com>
2025-05-13 14:11:00 -0300

    Actualizada la lista de comedores para usar admisiones en lugar de comedores; modificar vista y plantilla correspondiente

--- acompanamientos/views.py
+++ acompanamientos/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from acompanamientos.models.acompanamiento import InformacionRelevante, Prestacion
+from admisiones.models.admisiones import Admision
 from comedores.models.comedor import Comedor
 
 def detalle_acompanamiento(request, comedor_id):
@@ -14,5 +15,5 @@ def detalle_acompanamiento(request, comedor_id):
     )
 
 def lista_comedores_acompanamiento(request):
-    comedores = Comedor.objects.filter(estado="Admitido - pendiente ejecución")
-    return render(request, "lista_comedores.html", {"comedores": comedores})
+    admisiones = Admision.objects.filter(estado__nombre="Test")
+    return render(request, "lista_comedores.html", {"admisiones": admisiones})
