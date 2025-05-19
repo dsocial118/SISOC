2ebba896990191c4da8e1d01c6320b8dcbf201e0
MariaNavarro90 <navarromariavictoria8@gmail.com>
2025-05-19 11:49:46 -0300

    Refactorizadas las vistas para utilizar vistas basadas en clases (CBV) para el detalle y listado de Acompañamiento; actualizado el manejo del contexto en los templates y el enrutamiento de URLs. Se Aplico filtro por dupla

--- acompanamientos/views.py
+++ acompanamientos/views.py
@@ -1,49 +1,79 @@
 from django.shortcuts import render, get_object_or_404
+from django.views.generic import ListView, DetailView
 from acompanamientos.models.acompanamiento import InformacionRelevante, Prestacion
 from admisiones.models.admisiones import Admision
 from comedores.models.comedor import Comedor
 from comedores.models.relevamiento import Relevamiento
 from acompanamientos.acompanamiento_service import AcompanamientoService
+from django.db.models import Q
+
 
 # TODO: Sincronizar con la tarea de Pablo y migrar a clases
-def detalle_acompanamiento(request, comedor_id):
-    comedor = get_object_or_404(Comedor, pk=comedor_id)
-    hitos = AcompanamientoService.obtener_hitos(comedor)
-    info_relevante = InformacionRelevante.objects.filter(comedor=comedor).first()
-    relevamiento = (
-        Relevamiento.objects.filter(comedor=comedor).order_by("-fecha_visita").first()
-    )
-    prestacion = (
-        relevamiento.prestacion if relevamiento and relevamiento.prestacion else None
-    )
-    dias = ["lunes", "martes", "miercoles", "jueves", "viernes", "sabado", "domingo"]
+class AcompanamientoDetailView(DetailView):
+    model = Comedor
+    template_name = "acompañamiento_detail.html"
+    context_object_name = "comedor"
+    pk_url_kwarg = "comedor_id"
 
-    prestaciones_dias = []
-    if prestacion:
-        for dia in dias:
-            prestaciones_dias.append(
-                {
-                    "dia": dia,
-                    "desayuno": getattr(prestacion, f"{dia}_desayuno_actual", "-"),
-                    "almuerzo": getattr(prestacion, f"{dia}_almuerzo_actual", "-"),
-                    "merienda": getattr(prestacion, f"{dia}_merienda_actual", "-"),
-                    "cena": getattr(prestacion, f"{dia}_cena_actual", "-"),
-                }
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        comedor = self.object
+        context["hitos"] = AcompanamientoService.obtener_hitos(comedor)
+        context["info_relevante"] = InformacionRelevante.objects.filter(comedor=comedor).first()
+        relevamiento = (
+            Relevamiento.objects.filter(comedor=comedor).order_by("-fecha_visita").first()
+        )
+        prestacion = (
+            relevamiento.prestacion if relevamiento and relevamiento.prestacion else None
+        )
+        dias = ["lunes", "martes", "miercoles", "jueves", "viernes", "sabado", "domingo"]
+
+        prestaciones_dias = []
+        if prestacion:
+            for dia in dias:
+                prestaciones_dias.append(
+                    {
+                        "dia": dia,
+                        "desayuno": getattr(prestacion, f"{dia}_desayuno_actual", "-"),
+                        "almuerzo": getattr(prestacion, f"{dia}_almuerzo_actual", "-"),
+                        "merienda": getattr(prestacion, f"{dia}_merienda_actual", "-"),
+                        "cena": getattr(prestacion, f"{dia}_cena_actual", "-"),
+                    }
+                )
+        context["prestaciones_dias"] = prestaciones_dias
+        return context
+
+
+class ComedoresAcompanamientoListView(ListView):
+    model = Admision
+    template_name = "lista_comedores.html"
+    context_object_name = "admisiones"
+
+    def get_queryset(self):
+        user = self.request.user
+        # TODO: Sincronizar estado con la tarea de Pablo
+
+        if user.is_superuser:
+            return (
+                Admision.objects.filter(estado__nombre="Test")
+                .values(
+                    "comedor__id",
+                    "comedor__nombre",
+                )
+                .distinct()
             )
-
-    return render(
-        request,
-        "acompañamiento_detail.html",
-        {
-            "comedor": comedor,
-            "info_relevante": info_relevante,
-            "prestaciones_dias": prestaciones_dias,
-            "hitos": hitos,
-        },
-    )
-# TODO : migrar a clases
-def lista_comedores_acompanamiento(request):
-    admisiones = Admision.objects.filter(estado__nombre="Test")
-    # TODO: Sincronizar estado con la tarea de Pablo
-
-    return render(request, "lista_comedores.html", {"admisiones": admisiones})
+        else:
+            return (
+                Admision.objects.filter(
+                    Q(estado__nombre="Test")
+                    & (
+                        Q(comedor__dupla__abogado=user)
+                        | Q(comedor__dupla__tecnico=user)
+                    )
+                )
+                .values(
+                    "comedor__id",
+                    "comedor__nombre",
+                )
+                .distinct()
+            )
