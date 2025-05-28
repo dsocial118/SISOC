ce3bdbe7c8b1448774d460a89592c2426c5d3925
juanikitro <jpkitro@gmail.com>
2025-05-28 15:21:56 -0400

    Añadi soporte para visualizar PDFs en las imagenes del relevamiento

--- configuraciones/templatetags/custom_filters.py
+++ configuraciones/templatetags/custom_filters.py
@@ -10,3 +10,11 @@ def has_group(user, group_name):
         return True
     # Si el usuario pertenece al grupo específico solicitado, retorna True
     return user.groups.filter(name=group_name).exists()
+
+
+@register.filter
+def endswith(value, suffix):
+    try:
+        return str(value).lower().endswith(str(suffix).lower())
+    except Exception:
+        return False
