9a40bef4ebe8e1e08609f9fc58b40514c408f1a4
MariaNavarro90 <navarromariavictoria8@gmail.com>
2025-06-18 18:27:17 -0300

    Agregado módulo core con modelos, vistas, decoradores y configuración de administración

---
+++ core/admin.py
@@ -0,0 +1,21 @@
+from django.contrib import admin
+
+from core.models import (
+    Localidad,
+    Provincia,
+    Municipio,
+    Sexo,
+    Mes,
+    Dia,
+    Turno,
+)
+
+admin.site.register(Provincia)
+admin.site.register(Municipio)
+admin.site.register(Localidad)
+admin.site.register(Sexo)
+admin.site.register(Mes)
+admin.site.register(Dia)
+admin.site.register(Turno)
+
+# Register your models here.
