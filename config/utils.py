ccdc3a7ec830815461569eac3bcc8e2c7d61e07f
MariaNavarro90 <navarromariavictoria8@gmail.com>
2025-06-26 12:14:13 -0300

    Migrado el uso de utilidades y decoradores a core, eliminando módulos obsoletos y añadiendo advertencias de deprecación.

--- config/utils.py
+++ config/utils.py
@@ -1,43 +1,8 @@
-import logging
-from datetime import datetime
-from pathlib import Path
+from core.utils import *  # noqa
 
-from django.utils import timezone
-
-
-class DailyFileHandler(logging.FileHandler):
-    def __init__(self, filename, mode="a", encoding=None, delay=False):
-        current_date = datetime.now().strftime("%Y-%m-%d")
-        daily_folder = Path(filename).parent / current_date
-        daily_folder.mkdir(parents=True, exist_ok=True)
-        daily_filename = daily_folder / Path(filename).name
-        super().__init__(daily_filename, mode, encoding, delay)
-
-
-def convert_string_to_int(value):
-    return int(value) if value != "" else None
-
-
-def format_fecha_gestionar(fecha_visita: datetime):
-    return fecha_visita.strftime("%d/%m/%Y %H:%M")
-
-
-def format_fecha_django(fecha_visita: str):
-    fecha_formateada = datetime.strptime(fecha_visita, "%d/%m/%Y %H:%M")
-    return timezone.make_aware(fecha_formateada, timezone.get_default_timezone())
-
-
-def format_serializer_errors(serializer):
-    error_messages = []
-    for field, errors in serializer.errors.items():
-        for error in errors:
-            if field == "non_field_errors":
-                # Errores sin campo específico
-                error_messages.append(str(error))
-            else:
-                error_messages.append(
-                    f"{field}: {str(error)}"
-                )  # Errores en campos específicos
-
-    error_message_str = " | ".join(error_messages)
-    return error_message_str
+import warnings
+warnings.warn(
+    "config.utils is deprecated. Use core.utils instead",
+    DeprecationWarning,
+    stacklevel=2
+)
--- configuraciones/decorators.py
+++ configuraciones/decorators.py
@@ -1,26 +1,13 @@
-from functools import wraps
+"""
+DEPRECATED MODULE - Use core.decorators instead
+All decorators migrated to core.decorators
+"""
 
-from django.core.exceptions import PermissionDenied
+import warnings
+from core.decorators import *  # noqa
 
-
-def group_required(group_names):
-    """
-    Permite el acceso solo a usuarios autenticados que pertenezcan a alguno de los grupos indicados,
-    o que sean superusuarios.
-    """
-
-    def in_group(user):
-        return user.is_authenticated and (
-            user.groups.filter(name__in=group_names).exists() or user.is_superuser
-        )
-
-    def decorator(view_func):
-        @wraps(view_func)
-        def _wrapped_view(request, *args, **kwargs):
-            if not in_group(request.user):
-                raise PermissionDenied
-            return view_func(request, *args, **kwargs)
-
-        return _wrapped_view
-
-    return decorator
+warnings.warn(
+    "configuraciones.decorators is deprecated. Use core.decorators instead", 
+    DeprecationWarning,
+    stacklevel=2
+)
