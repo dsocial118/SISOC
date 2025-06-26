f3638930ecf1e6b2485b516b5e2f8d0c6e74102a
MariaNavarro90 <navarromariavictoria8@gmail.com>
2025-06-26 10:04:37 -0300

    Eliminada la línea de comentario y reestructurada la importación de modelos en admin.py

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
 
-# Register your models here.
+from core.models import (
+    Localidad,
+    Provincia,
+    Municipio,
+    Sexo,
+    Mes,
+    Dia,
+    Turno,
+)
+
+admin.site.register(Provincia)
+admin.site.register(Municipio)
+admin.site.register(Localidad)
+admin.site.register(Sexo)
+admin.site.register(Mes)
+admin.site.register(Dia)
+admin.site.register(Turno)
