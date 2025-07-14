b498af973517cbef80eb94a4eaff556cdec5e4d2
juanikitro <jpkitro@gmail.com>
2025-07-14 11:38:30 -0500

    arreglo de grouprequired y eliminacion de reduncancia en urls

--- cdi/urls.py
+++ cdi/urls.py
@@ -12,27 +12,27 @@ from core.decorators import group_required
 urlpatterns = [
     path(
         "cdi/listar",
-        group_required("CDI")(CDIListView.as_view()),
+        group_required(["CDI"])(CDIListView.as_view()),
         name="cdi",
     ),
     path(
         "cdi/crear",
-        group_required("CDI")(CDICreateView.as_view()),
+        group_required(["CDI"])(CDICreateView.as_view()),
         name="cdi_crear",
     ),
     path(
         "cdi/detalle/<int:pk>",
-        group_required("CDI")(CDIDetailView.as_view()),
+        group_required(["CDI"])(CDIDetailView.as_view()),
         name="cdi_detalle",
     ),
     path(
         "cdi/editar/<int:pk>",
-        group_required("CDI")(CDIUpdateView.as_view()),
+        group_required(["CDI"])(CDIUpdateView.as_view()),
         name="cdi_editar",
     ),
     path(
         "cdi/eliminar/<int:pk>",
-        group_required("CDI")(CDIDeleteView.as_view()),
+        group_required(["CDI"])(CDIDeleteView.as_view()),
         name="cdi_eliminar",
     ),
 ]
