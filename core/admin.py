61b90a0a5c5e699ec1a296738a90718e06c1eeac
dsocial118 <167777359+dsocial118@users.noreply.github.com>
2025-07-21 11:51:31 -0500

    Update admin.py

--- core/admin.py
+++ core/admin.py
@@ -12,8 +12,19 @@ from core.models import (
 
 admin.site.register(Provincia)
 admin.site.register(Municipio)
-admin.site.register(Localidad)
 admin.site.register(Sexo)
 admin.site.register(Mes)
 admin.site.register(Dia)
 admin.site.register(Turno)
+
+
+@admin.register(Localidad)
+class LocalidadAdmin(admin.ModelAdmin):
+    def formfield_for_foreignkey(self, db_field, request, **kwargs):
+        if db_field.name == "municipio":
+            provincia_id = request.GET.get("provincia")
+            if provincia_id:
+                kwargs["queryset"] = Municipio.objects.filter(provincia_id=provincia_id)
+            else:
+                kwargs["queryset"] = Municipio.objects.all()
+        return super().formfield_for_foreignkey(db_field, request, **kwargs)
