b4ac97b85fe90560b938605fafa5a5a48f6b326e
juanikitro <jpkitro@gmail.com>
2025-07-23 17:03:42 -0500

    format

--- celiaquia/models.py
+++ celiaquia/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.contrib.auth import get_user_model
-from ciudadanos.models import  Ciudadano
+from ciudadanos.models import Ciudadano
 
 User = get_user_model()
 
