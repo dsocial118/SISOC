65979e8d13fcd6bf333dea124f074485513cd6ef
matias394 <farinamatias00@gmail.com>
2025-08-01 15:27:29 -0300

    PR LEPE

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,7 +1,7 @@
 from django.views.generic import TemplateView
 
 from dashboard.models import Dashboard
-
+from centrodefamilia.models import Centro, ActividadCentro, ParticipanteActividad
 
 class DashboardView(TemplateView):
     template_name = "dashboard.html"
@@ -9,9 +9,14 @@ class DashboardView(TemplateView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
 
+        # 1) Cargar datos fijos desde tu modelo Dashboard
         dashboard_data = Dashboard.objects.all()
-        data = {item.llave: item.cantidad for item in dashboard_data}
+        context.update({ item.llave: item.cantidad for item in dashboard_data })
 
-        context.update(data)
+        # 2) Calcular dinámicamente los indicadores que necesitas
+        context['participantes_total'] = ParticipanteActividad.objects.count()
+        context['centros_adheridos_totales'] = Centro.objects.filter(tipo='adherido').count()
+        context['centros_faro_totales']    = Centro.objects.filter(tipo='faro').count()
+        context['actividades_totales']      = ActividadCentro.objects.count()
 
         return context
