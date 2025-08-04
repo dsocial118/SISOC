b646aa6fcc13f65e58371ab4b2af82d2b6acf840
matias394 <farinamatias00@gmail.com>
2025-08-04 09:43:01 -0300

    VersionEstable

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,6 +1,7 @@
 from django.views.generic import TemplateView
 
 from dashboard.models import Dashboard
+from centrodefamilia.models import Centro, ActividadCentro, ParticipanteActividad
 
 
 class DashboardView(TemplateView):
@@ -9,9 +10,23 @@ class DashboardView(TemplateView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
 
+        # 1) Datos fijos
         dashboard_data = Dashboard.objects.all()
-        data = {item.llave: item.cantidad for item in dashboard_data}
+        context.update({item.llave: item.cantidad for item in dashboard_data})
 
-        context.update(data)
+        # 2) Indicadores dinámicos
+        context["participantes_total"] = ParticipanteActividad.objects.filter(
+            estado="inscrito"
+        ).count()
+
+        context["centros_adheridos_totales"] = Centro.objects.filter(
+            tipo="adherido"
+        ).count()
+
+        context["centros_faro_totales"] = Centro.objects.filter(
+            tipo="faro"
+        ).count()
+
+        context["actividades_totales"] = ActividadCentro.objects.count()
 
         return context
