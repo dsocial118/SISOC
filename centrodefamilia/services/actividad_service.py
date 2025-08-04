64e2aab592da5bf228897dadf9bcf919cab2706a
Nenu <nehuennavarro@gmail.com>
2025-08-04 09:07:54 -0300

    cambios en servicios

--- centrodefamilia/services/actividad_service.py
+++ centrodefamilia/services/actividad_service.py
@@ -1,9 +1,15 @@
 from centrodefamilia.models import Actividad
+import logging
 
+logger = logging.getLogger(__name__)
 
 def actividades_disponibles_para_centro():
     """
     Devuelve todas las actividades cargadas por el técnico
     (puede extenderse para aplicar filtros por centro en el futuro).
     """
-    return Actividad.objects.all()
+    try:
+        return Actividad.objects.all()
+    except Exception as e:
+        logger.error("Ocurrió un error inesperado en actividades_disponibles_para_centro", exc_info=True)
+        return Actividad.objects.none()
