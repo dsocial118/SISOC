b46fcecad437cf189ee9490b4be3d7754b488ced
dsocial118 <167777359+dsocial118@users.noreply.github.com>
2025-08-05 20:51:10 -0500

    Update custom_filters.py

--- core/templatetags/custom_filters.py
+++ core/templatetags/custom_filters.py
@@ -5,12 +5,12 @@ register = template.Library()
 
 @register.filter
 def has_group(user, group_name):
-    if not user:
+    try:
+        if not hasattr(user, "cached_groups"):
+            user.cached_groups = list(user.groups.values_list("name", flat=True))
+        return group_name in user.cached_groups
+    except Exception:
         return False
-    if not hasattr(user, "cached_groups"):
-        user.cached_groups = list(user.groups.values_list("name", flat=True))
-    return group_name in user.cached_groups
-
 
 @register.filter
 def endswith(value, suffix):
