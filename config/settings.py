4d3c2b80318105df4fb013c2fa82045d26d6f202
dsocial118 <167777359+dsocial118@users.noreply.github.com>
2025-08-08 16:41:54 -0500

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -1,9 +1,9 @@
 import os
+import logging
 from pathlib import Path
 import sys
 from django.contrib.messages import constants as messages
 from dotenv import load_dotenv
-
 
 # Cargar variables de entorno desde el archivo .env
 load_dotenv()
@@ -228,50 +228,85 @@ CENTROFAMILIA_CACHE_TIMEOUT = 300  # 5 minutos para centro de familia
 
 
 # Configuracion de logging
-# LOGGING = {
-#     "version": 1,
-#     "disable_existing_loggers": False,
-#     "formatters": {
-#         "verbose": {
-#             "format": "[{asctime}] {module} {levelname} {name}: {message}",
-#             "style": "{",
-#         },
-#         "simple": {
-#             "format": "[{asctime}] {levelname} {message}",
-#             "style": "{",
-#         },
-#     },
-#     "handlers": {
-#         "file_info": {
-#             "class": "core.utils.DailyFileHandler",
-#             "level": "INFO",
-#             "formatter": "verbose",
-#             "filename": str(BASE_DIR / "logs/info.log"),
-#         },
-#         "console": {
-#             "class": "logging.StreamHandler",
-#             "level": "WARNING",
-#             "formatter": "simple",
-#         },
-#     },
-#     "loggers": {
-#         "django": {
-#             "handlers": ["file_info", "console"],
-#             "level": "INFO",
-#             "propagate": False,
-#         },
-#         "django.request": {
-#             "handlers": ["console"],
-#             "level": "WARNING",
-#             "propagate": False,
-#         },
-#     },
-#     "root": {
-#         "handlers": ["console"],
-#         "level": "WARNING",
-#     },
-# }
-
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "filters": {
+        "info_only": {
+            "()": "django.utils.log.CallbackFilter",
+            "callback": lambda r: r.levelno == logging.INFO,
+        },
+        "error_only": {
+            "()": "django.utils.log.CallbackFilter",
+            "callback": lambda r: r.levelno == logging.ERROR,
+        },
+        "warning_only": {
+            "()": "django.utils.log.CallbackFilter",
+            "callback": lambda r: r.levelno == logging.WARNING,
+        },
+        "critical_only": {
+            "()": "django.utils.log.CallbackFilter",
+            "callback": lambda r: r.levelno == logging.CRITICAL,
+        },
+    },
+    "formatters": {
+        "verbose": {
+            "format": "[{asctime}] {module} {levelname} {name}: {message}",
+            "style": "{",
+        },
+        "simple": {
+            "format": "[{asctime}] {levelname} {message}",
+            "style": "{",
+        },
+    },
+    "handlers": {
+        "info_file": {
+            "level": "INFO",
+            "filters": ["info_only"],
+            "class": "core.utils.DailyFileHandler",
+            "filename": str(BASE_DIR / "logs/info.log"),
+            "formatter": "verbose",
+        },
+        "error_file": {
+            "level": "ERROR",
+            "filters": ["error_only"],
+            "class": "core.utils.DailyFileHandler",
+            "filename": str(BASE_DIR / "logs/error.log"),
+            "formatter": "verbose",
+        },
+        "warning_file": {
+            "level": "WARNING",
+            "filters": ["warning_only"],
+            "class": "core.utils.DailyFileHandler",
+            "filename": str(BASE_DIR / "logs/warning.log"),
+            "formatter": "verbose",
+        },
+        "critical_file": {
+            "level": "CRITICAL",
+            "filters": ["critical_only"],
+            "class": "core.utils.DailyFileHandler",
+            "filename": str(BASE_DIR / "logs/critical.log"),
+            "formatter": "verbose",
+        },
+    },
+    "loggers": {
+        "django": {
+            "handlers": [
+                "info_file",
+                "error_file",
+                "warning_file",
+                "critical_file",
+            ],
+            "level": "DEBUG",
+            "propagate": True,
+        },
+        "django.request": {
+            "handlers": ["error_file"],
+            "level": "ERROR",
+            "propagate": False,
+        },
+    },
+}
 
 # Configuración de validadores de contraseñas
 AUTH_PASSWORD_VALIDATORS = [
@@ -307,6 +342,9 @@ if DEBUG:
 
 if ENVIRONMENT == "prd":
     # Configuración para producción
+    STATICFILES_STORAGE = (
+        "django.contrib.staticfiles.storage.ManifestStaticFilesStorage"
+    )
     SECURE_HSTS_SECONDS = 31536000
     SECURE_HSTS_INCLUDE_SUBDOMAINS = False
     SECURE_SSL_REDIRECT = True
@@ -314,6 +352,7 @@ if ENVIRONMENT == "prd":
     CSRF_COOKIE_SECURE = True
 else:
     # Configuración para entornos bajos (no ssl)
+    STATICFILES_STORAGE = "django.contrib.staticfiles.storage.StaticFilesStorage"
     SECURE_HSTS_SECONDS = 0
     SECURE_HSTS_INCLUDE_SUBDOMAINS = False
     SECURE_SSL_REDIRECT = False
@@ -336,20 +375,3 @@ DOMINIO = os.environ.get("DOMINIO", default="localhost:8001")
 RENAPER_API_USERNAME = os.getenv("RENAPER_API_USERNAME")
 RENAPER_API_PASSWORD = os.getenv("RENAPER_API_PASSWORD")
 RENAPER_API_URL = os.getenv("RENAPER_API_URL")
-
-
-LOGGING = {
-    "version": 1,
-    "disable_existing_loggers": False,
-    "handlers": {
-        "console": {"class": "logging.StreamHandler"},
-    },
-    "loggers": {
-        "django": {"handlers": ["console"], "level": "ERROR", "propagate": False},
-        "django.request": {
-            "handlers": ["console"],
-            "level": "ERROR",
-            "propagate": False,
-        },
-    },
-}
