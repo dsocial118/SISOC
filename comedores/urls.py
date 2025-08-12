0eb7c9c6f9461a5862bf75eccf1397b5f362fdb1
Pablo <pablocao@gmail.com>
2025-08-12 09:12:40 -0300

    Se agrego para poder agregar Ciudadano nuevo cuando se agrega una persona a la Nomina. Ademas se hicieron unos arreglos en la parte del detail de centro de familia que no funcionaba el acordeon js

--- comedores/urls.py
+++ comedores/urls.py
@@ -13,9 +13,9 @@ from comedores.views import (
     NominaDetailView,
     NominaCreateView,
     NominaDeleteView,
-    NominaUpdateView,
     AsignarDuplaListView,
     relevamiento_crear_editar_ajax,
+    nomina_editar_ajax,
 )
 
 from intervenciones.views import (
@@ -147,14 +147,14 @@ urlpatterns = [
         name="nomina_ver",
     ),
     path(
+        "comedores/editar-nomina/<int:pk>/",
+        nomina_editar_ajax,
+        name="nomina_editar_ajax",
+    ),
+    path(
         "comedores/<int:pk>/nomina/crear/",
         group_required(["Comedores Nomina Crear"])(NominaCreateView.as_view()),
         name="nomina_crear",
-    ),
-    path(
-        "comedores/<int:pk>/nomina/<int:pk2>/editar/",
-        group_required(["Comedores Nomina Editar"])(NominaUpdateView.as_view()),
-        name="nomina_editar",
     ),
     path(
         "comedores/<int:pk>/nomina/<int:pk2>/eliminar/",
