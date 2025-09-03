d815322db0e59523b4cfb93e37639498e1733a2e
nehuen871 <nehuennavarro@gmail.com>
2025-09-03 11:45:30 -0300

    cambios

--- organizaciones/models.py
+++ organizaciones/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from core.models import Municipio, Provincia, Localidad
+
+
 class TipoOrganizacion(models.Model):
     nombre = models.CharField(max_length=255, unique=True)
 
