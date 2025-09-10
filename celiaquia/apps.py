ef1c8a3c53b994689fb78c1fd7720d925c947dc7
Mkdir-arg <farinamatias00@gmail.com>
2025-09-10 11:50:27 -0300

    Add expediente state history tracking

--- celiaquia/apps.py
+++ celiaquia/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class CeliaquiaConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "celiaquia"
+
+    def ready(self):  # pragma: no cover - Django startup
+        from . import signals  # pylint: disable=import-outside-toplevel, unused-import
