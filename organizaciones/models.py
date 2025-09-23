a3fcacf5e3c2861c2f46a01bba567aadf51dc6b7
nehuen871 <nehuennavarro@gmail.com>
2025-09-23 11:14:52 -0300

    cambios

--- organizaciones/models.py
+++ organizaciones/models.py
@@ -1,7 +1,7 @@
 from django.db import models
-from core.models import Municipio, Provincia, Localidad
-from django.core.validators import MaxValueValidator, MinValueValidator
 from django.utils import timezone
+from django.core.validators import MaxValueValidator, MinValueValidator
+from core.models import Municipio, Provincia, Localidad
 
 
 class TipoOrganizacion(models.Model):
