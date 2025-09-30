34dfff58784907f1c997bf597fa0355c89e2fe9a
MariaNavarro90 <navarromariavictoria8@gmail.com>
2025-09-30 09:58:38 -0300

    creado el bloque inicio

--- core/urls.py
+++ core/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
 
 from .views import (
+    inicio_view,
     load_localidad,
     load_municipios,
 )
 
 urlpatterns = [
+    path("inicio/", inicio_view, name="inicio"),
     path("ajax/load-municipios/", load_municipios, name="ajax_load_municipios"),
     path("ajax/load-localidades/", load_localidad, name="ajax_load_localidades"),
 ]
