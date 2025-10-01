b3c8e76383a26cd02e1e3e3edb4321ca3dda1dc5
Juan Ignacio Portilla Kitroser <jpkitro@gmail.com>
2025-09-30 22:08:52 -0300

    Fix URL templates for expediente detail actions

--- static/custom/js/expediente_detail_config.js
+++ static/custom/js/expediente_detail_config.js
@@ -3,6 +3,9 @@ window.PROCESS_URL = document.querySelector('meta[name="process-url"]')?.content
 window.CONFIRM_URL = document.querySelector('meta[name="confirm-url"]')?.content;
 window.CRUCE_URL = document.querySelector('meta[name="cruce-url"]')?.content;
 window.CSRF_TOKEN = document.querySelector('meta[name="csrf-token"]')?.content;
-window.REVISAR_URL_TEMPLATE = document.querySelector('meta[name="revisar-url-template"]')?.content;
+
+const revisarMeta = document.querySelector('meta[name="revisar-url-template"]');
+window.REVISAR_URL_TEMPLATE = revisarMeta?.content?.replace('/0/', '/{id}/');
 window.CONFIRM_SUBS_URL = document.querySelector('meta[name="confirm-subs-url"]')?.content;
-window.VALIDAR_RENAPER_URL_TEMPLATE = document.querySelector('meta[name="validar-renaper-url-template"]')?.content;
+const validarMeta = document.querySelector('meta[name="validar-renaper-url-template"]');
+window.VALIDAR_RENAPER_URL_TEMPLATE = validarMeta?.content?.replace('/0/', '/{id}/');
