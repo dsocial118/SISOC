b392bec8fb8565fa500a98f4fd0000b938243c0d
juanikitro <jpkitro@gmail.com>
2025-11-04 01:13:33 +0800

    Fix toomanyconnections en 500 y admisiones detail

--- config/views.py
+++ config/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
+from django.http import HttpResponseServerError
+from django.template import loader
 
 
-def server_error(request, template_name="500.html"):
-    """Render the 500 error page with the request in the context."""
-    return render(request, template_name, status=500)
+def server_error(_request, template_name="500.html"):
+    """Return a static 500 error response without invoking context processors."""
+    template = loader.get_template(template_name)
+    return HttpResponseServerError(template.render())
