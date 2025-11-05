099f823c9baa7ca41b415ed8c7d55d4e326795d1
nehuen871 <nehuennavarro@gmail.com>
2025-11-05 15:23:22 -0300

    cambios duplas

--- duplas/urls.py
+++ duplas/urls.py
@@ -10,27 +10,27 @@ from duplas.views import (
 
 urlpatterns = [
     path(
-        "duplas/",
+        "equipo-tecnico/",
         group_required(["Admin"])(DuplaListView.as_view()),
         name="dupla_list",
     ),
     path(
-        "duplas/crear/",
+        "equipo-tecnico/crear/",
         group_required(["Admin"])(DuplaCreateView.as_view()),
         name="dupla_crear",
     ),
     path(
-        "duplas/<int:pk>/editar/",
+        "equipo-tecnico/<int:pk>/editar/",
         group_required(["Admin"])(DuplaUpdateView.as_view()),
         name="dupla_editar",
     ),
     path(
-        "duplas/<int:pk>/",
+        "equipo-tecnico/<int:pk>/",
         group_required(["Admin"])(DuplaDetailView.as_view()),
         name="dupla_detalle",
     ),
     path(
-        "duplas/<int:pk>/eliminar/",
+        "equipo-tecnico/<int:pk>/eliminar/",
         group_required(["Admin"])(DuplaDeleteView.as_view()),
         name="dupla_eliminar",
     ),
