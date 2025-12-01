14cc356b9658c996776100194e883f8362534b08
dsocial118 <167777359+dsocial118@users.noreply.github.com>
2025-12-01 10:29:06 -0300

    Update api_views.py

--- relevamientos/views/api_views.py
+++ relevamientos/views/api_views.py
@@ -6,8 +6,7 @@ from rest_framework.exceptions import ValidationError
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
-from rest_framework_api_key.permissions import HasAPIKey
-
+from core.api_auth import HasAPIKeyOrToken
 from core.utils import format_serializer_errors
 from relevamientos.models import Relevamiento
 from relevamientos.serializer import RelevamientoSerializer
