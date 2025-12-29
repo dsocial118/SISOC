34efd69ef654730fa5c4563b8b0528c81681fd0e
nehuen871 <nehuennavarro@gmail.com>
2025-12-29 17:27:53 -0300

    cambios

--- importarexpediente/urls.py
+++ importarexpediente/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import ImportExpedientesView, ImportarExpedienteListView, ImportarExpedienteDetalleListView
+from .views import ImportExpedientesView, ImportarExpedienteListView, ImportarExpedienteDetalleListView, ImportDatosView
 
 urlpatterns = [
     path("importarexpedientes/upload", ImportExpedientesView.as_view(), name="upload"),
     path("importarexpedientes/listar", ImportarExpedienteListView.as_view(), name="importarexpedientes_list"),
     path("importarexpedientes/<int:id_archivo>/", ImportarExpedienteDetalleListView.as_view(), name="importarexpediente_detail"),
+    path("importarexpedientes/importar_datos/", ImportDatosView.as_view(), name="importar_datos"),
 ]
