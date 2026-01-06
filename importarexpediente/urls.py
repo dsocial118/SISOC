d412dba69db4ae83bb820081e8ff54e4e07885d8
nehuen871 <nehuennavarro@gmail.com>
2026-01-06 12:05:25 -0300

    lint black

--- importarexpediente/urls.py
+++ importarexpediente/urls.py
@@ -1,10 +1,32 @@
 from django.urls import path
-from .views import ImportExpedientesView, ImportarExpedienteListView, ImportarExpedienteDetalleListView, ImportDatosView,BorrarDatosImportadosView
+from .views import (
+    ImportExpedientesView,
+    ImportarExpedienteListView,
+    ImportarExpedienteDetalleListView,
+    ImportDatosView,
+    BorrarDatosImportadosView,
+)
 
 urlpatterns = [
     path("importarexpedientes/upload", ImportExpedientesView.as_view(), name="upload"),
-    path("importarexpedientes/listar", ImportarExpedienteListView.as_view(), name="importarexpedientes_list"),
-    path("importarexpedientes/<int:id_archivo>/", ImportarExpedienteDetalleListView.as_view(), name="importarexpediente_detail"),
-    path("importarexpedientes/<int:id_archivo>/importar_datos/", ImportDatosView.as_view(), name="importar_datos"),
-    path("importarexpedientes/<int:id_archivo>/borrar_datos_importados/", BorrarDatosImportadosView.as_view(), name="borrar_datos_importados"),
+    path(
+        "importarexpedientes/listar",
+        ImportarExpedienteListView.as_view(),
+        name="importarexpedientes_list",
+    ),
+    path(
+        "importarexpedientes/<int:id_archivo>/",
+        ImportarExpedienteDetalleListView.as_view(),
+        name="importarexpediente_detail",
+    ),
+    path(
+        "importarexpedientes/<int:id_archivo>/importar_datos/",
+        ImportDatosView.as_view(),
+        name="importar_datos",
+    ),
+    path(
+        "importarexpedientes/<int:id_archivo>/borrar_datos_importados/",
+        BorrarDatosImportadosView.as_view(),
+        name="borrar_datos_importados",
+    ),
 ]
