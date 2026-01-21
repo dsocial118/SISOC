c8cbb84ac937afec3249f7ae0af301a3edb61752
dsocial118 <167777359+dsocial118@users.noreply.github.com>
2026-01-21 11:00:30 -0300

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -380,14 +380,10 @@ if ENVIRONMENT == "prd":
     )
     SECURE_HSTS_SECONDS = 1800  # 30 minutos
     SECURE_HSTS_INCLUDE_SUBDOMAINS = True
-    SECURE_HSTS_PRELOAD = False
     SECURE_SSL_REDIRECT = True
     SESSION_COOKIE_SECURE = True
-    SESSION_COOKIE_HTTPONLY = True
     SESSION_COOKIE_SAMESITE = "None"  # Permitir cookies en contexto cross-site para APIs externas
     CSRF_COOKIE_SECURE = True
-    CSRF_COOKIE_HTTPONLY = False
-    CSRF_COOKIE_SAMESITE = "Lax"
     USE_X_FORWARDED_HOST = True
     SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
     SECURE_CONTENT_TYPE_NOSNIFF = True
@@ -399,14 +395,10 @@ else:
     STATICFILES_STORAGE = "django.contrib.staticfiles.storage.StaticFilesStorage"
     SECURE_HSTS_SECONDS = 0
     SECURE_HSTS_INCLUDE_SUBDOMAINS = False
-    SECURE_HSTS_PRELOAD = False
     SECURE_SSL_REDIRECT = False
     SESSION_COOKIE_SECURE = False
-    SESSION_COOKIE_HTTPONLY = True
     SESSION_COOKIE_SAMESITE = "None"  # Permitir cookies en contexto cross-site para APIs externas
     CSRF_COOKIE_SECURE = False
-    CSRF_COOKIE_HTTPONLY = False
-    CSRF_COOKIE_SAMESITE = "Lax"
     SECURE_CONTENT_TYPE_NOSNIFF = True
     SECURE_BROWSER_XSS_FILTER = True
     X_FRAME_OPTIONS = "SAMEORIGIN"
