67e630061845d06ea7702998cbb8e8ec749836c9
MariaNavarro90 <navarromariavictoria8@gmail.com>
2026-03-12 14:15:28 -0300

    Fix a inicio de db en signal de pwa

--- pwa/signals.py
+++ pwa/signals.py
@@ -1,5 +1,11 @@
+from django.db.utils import ProgrammingError, OperationalError
+
 from pwa.catalogo_seed import bootstrap_catalogo_actividades
 
 
 def seed_catalogo_actividades(sender, **kwargs):
-    bootstrap_catalogo_actividades(using=kwargs.get("using", "default"))
+    try:
+        bootstrap_catalogo_actividades(using=kwargs.get("using", "default"))
+    except (ProgrammingError, OperationalError):
+        # La tabla aún no existe (DB nueva antes de que corran las migraciones de pwa).
+        pass
