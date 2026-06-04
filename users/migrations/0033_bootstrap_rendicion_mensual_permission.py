a99217b1462e88339aa09da39f44ed2df3ebae89
MariaNavarro90 <navarromariavictoria8@gmail.com>
2026-06-04 13:39:55 -0300

    Asigna view_rendicioncuentamensual a: Tecnico Comedor, Area Contable, Coordinador Equipo Tecnico, Coordinador general, Coordinador Gestion

---
+++ users/migrations/0033_bootstrap_rendicion_mensual_permission.py
@@ -0,0 +1,42 @@
+from django.db import migrations
+
+
+GROUPS_WITH_RENDICION_ACCESS = [
+    "Tecnico Comedor",
+    "Area Contable",
+    "Coordinador Equipo Tecnico",
+    "Coordinador general",
+    "Coordinador Gestion",
+]
+
+
+def assign_rendicion_mensual_permission(apps, schema_editor):
+    Group = apps.get_model("auth", "Group")
+    Permission = apps.get_model("auth", "Permission")
+
+    permission = Permission.objects.filter(
+        content_type__app_label="rendicioncuentasmensual",
+        codename="view_rendicioncuentamensual",
+    ).first()
+
+    if not permission:
+        return
+
+    for group_name in GROUPS_WITH_RENDICION_ACCESS:
+        group = Group.objects.filter(name=group_name).first()
+        if group:
+            group.permissions.add(permission)
+
+
+def noop_reverse(apps, schema_editor):
+    pass
+
+
+class Migration(migrations.Migration):
+    dependencies = [
+        ("users", "0032_merge_cdf_referente_profile_source"),
+    ]
+
+    operations = [
+        migrations.RunPython(assign_rendicion_mensual_permission, noop_reverse),
+    ]
