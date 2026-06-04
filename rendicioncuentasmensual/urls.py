818d04a3c5a913f611166e01a89fa65c4fc592de
MariaNavarro90 <navarromariavictoria8@gmail.com>
2026-06-04 13:34:06 -0300

    Todas las rutas ahora usan rendicioncuentasmensual.view_rendicioncuentamensual

--- rendicioncuentasmensual/urls.py
+++ rendicioncuentasmensual/urls.py
@@ -11,82 +11,56 @@ from rendicioncuentasmensual.views import (
     eliminar_archivo,
 )
 
+_RENDICION_PERM = ["rendicioncuentasmensual.view_rendicioncuentamensual"]
+
 urlpatterns = [
     path(
         "rendicioncuentasmensual/listado/",
-        permissions_any_required(
-            [
-                "comedores.view_comedor",
-                "admisiones.view_admision",
-                "acompanamientos.view_informacionrelevante",
-            ]
-        )(RendicionCuentaMensualGlobalListView.as_view()),
+        permissions_any_required(_RENDICION_PERM)(
+            RendicionCuentaMensualGlobalListView.as_view()
+        ),
         name="rendicioncuentasmensual_global_list",
     ),
     path(
         "rendicioncuentasmensual/<int:comedor_id>/",
-        permissions_any_required(
-            [
-                "comedores.view_comedor",
-                "admisiones.view_admision",
-                "acompanamientos.view_informacionrelevante",
-            ]
-        )(RendicionCuentaMensualListView.as_view()),
+        permissions_any_required(_RENDICION_PERM)(
+            RendicionCuentaMensualListView.as_view()
+        ),
         name="rendicioncuentasmensual_list",
     ),
     path(
         "rendicioncuentasmensual/detalle/<int:pk>/",
-        permissions_any_required(
-            [
-                "comedores.view_comedor",
-                "admisiones.view_admision",
-                "acompanamientos.view_informacionrelevante",
-            ]
-        )(RendicionCuentaMensualDetailView.as_view()),
+        permissions_any_required(_RENDICION_PERM)(
+            RendicionCuentaMensualDetailView.as_view()
+        ),
         name="rendicioncuentasmensual_detail",
     ),
     path(
         "rendicioncuentasmensual/detalle/<int:pk>/descargar-pdf/",
-        permissions_any_required(
-            [
-                "comedores.view_comedor",
-                "admisiones.view_admision",
-                "acompanamientos.view_informacionrelevante",
-            ]
-        )(RendicionCuentaMensualDownloadPdfView.as_view()),
+        permissions_any_required(_RENDICION_PERM)(
+            RendicionCuentaMensualDownloadPdfView.as_view()
+        ),
         name="rendicioncuentasmensual_download_pdf",
     ),
     path(
         "rendicioncuentasmensual/eliminar/<int:pk>/",
-        permissions_any_required(
-            [
-                "comedores.view_comedor",
-                "admisiones.view_admision",
-                "acompanamientos.view_informacionrelevante",
-            ]
-        )(RendicionCuentaMensualDeleteView.as_view()),
+        permissions_any_required(_RENDICION_PERM)(
+            RendicionCuentaMensualDeleteView.as_view()
+        ),
         name="rendicioncuentasmensual_delete",
     ),
     path(
         "rendicioncuentasmensual/nuevo/<int:comedor_id>/",
-        permissions_any_required(
-            [
-                "comedores.view_comedor",
-                "admisiones.view_admision",
-                "acompanamientos.view_informacionrelevante",
-            ]
-        )(RendicionCuentaMensualCreateView.as_view()),
+        permissions_any_required(_RENDICION_PERM)(
+            RendicionCuentaMensualCreateView.as_view()
+        ),
         name="rendicioncuentasmensual_create",
     ),
     path(
         "rendicioncuentasmensual/editar/<int:pk>/",
-        permissions_any_required(
-            [
-                "comedores.view_comedor",
-                "admisiones.view_admision",
-                "acompanamientos.view_informacionrelevante",
-            ]
-        )(RendicionCuentaMensualUpdateView.as_view()),
+        permissions_any_required(_RENDICION_PERM)(
+            RendicionCuentaMensualUpdateView.as_view()
+        ),
         name="rendicioncuentasmensual_update",
     ),
     path(
