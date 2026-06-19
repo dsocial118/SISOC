b3c6d542abc05ffcaf1e88814e09e20f18af29e8
MariaNavarro90 <navarromariavictoria8@gmail.com>
2026-06-19 11:25:31 -0300

    Agregado campo departamento_contacto = CharField(max_length=255, blank=True, null=True) en el modelo Trabajador, entre tipo_barrio y municipio_contacto.

--- centrodeinfancia/models.py
+++ centrodeinfancia/models.py
@@ -953,6 +953,12 @@ class Trabajador(SoftDeleteModelMixin, models.Model):
         related_name="+",
         verbose_name="Jurisdicción",
     )
+    departamento_contacto = models.CharField(
+        max_length=255,
+        blank=True,
+        null=True,
+        verbose_name="Departamento",
+    )
     municipio_contacto = models.ForeignKey(
         "core.Municipio",
         on_delete=models.SET_NULL,
