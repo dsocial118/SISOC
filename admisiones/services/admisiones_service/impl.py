b24945c6dbb1bdcf32190a9805898c823e9b61a9
juanikitro <jpkitro@gmail.com>
2026-09-16 18:06:21 -0300

    Revert "Merge pull request #2465 from dsocial118/providencias_tk2326"
    This reverts commit 33123bf66f839fffba0bcc3610329d265088ec15, reversing
    changes made to ebb222bc64bbe486fbdaaf3aae52f3bc1acfccbe.

--- admisiones/services/admisiones_service/impl.py
+++ admisiones/services/admisiones_service/impl.py
@@ -3323,11 +3323,9 @@ class AdmisionService:
 
     @staticmethod
     def _append_botones_generales_admision(botones, admision, es_tecnico):
-        # Antes se habilitaba con la Disposición cargada. Eliminada la
-        # Disposición, el hito equivalente pasa a ser el Convenio.
         if (
             es_tecnico
-            and admision.numero_convenio
+            and admision.numero_disposicion
             and not admision.enviado_acompaniamiento
         ):
             botones.append("comenzar_acompaniamiento")
